220d1a665bd93750989d3a2c77912ccd0f6bfd99
Naviary <163621561+Naviary2@users.noreply.github.com>
2024-07-10 15:09:56 -0600

    reverted path

--- src/server/utility/HTMLScriptInjector.js
+++ src/server/utility/HTMLScriptInjector.js
@@ -93,13 +93,13 @@ function getCachedHTML(htmlFilePath) {
 
 { // Inject into play.html, our OBFUSCATED htmlscript.js script.
     const htmlFilePath = path.join(__dirname, '..', '..', '..', 'dist', 'views', 'play.html');
-    const jsFilePath = path.join(__dirname, '..', '..', 'client', 'scripts', 'game', 'htmlscript.js');
+    const jsFilePath = path.join(__dirname, '..', '..', '..', 'dist', 'scripts', 'game', 'htmlscript.js');
     prepareAndCacheHTML(htmlFilePath, jsFilePath, '<head>');
 }
 
 { // Inject into dev.html, our htmlscript.js script.
     const htmlFilePath = path.join(__dirname, '..', '..', '..', 'dist', 'views', 'dev.html');
-    const jsFilePath = path.join(__dirname, '..', '..', 'client', 'scripts', 'game', 'htmlscript.js');
+    const jsFilePath = path.join(__dirname, '..', '..', '..', 'dist', 'scripts', 'game', 'htmlscript.js');
     prepareAndCacheHTML(htmlFilePath, jsFilePath, '<head>');
 }
 
