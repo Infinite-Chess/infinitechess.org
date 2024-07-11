a11c09c7a28bcb6128d14dbcfcc014afe52830d8
Heinrich-XIAO <74563446+Heinrich-XIAO@users.noreply.github.com>
2024-07-11 20:40:42 +0000

    Remove upper password length bound.

--- src/server/controllers/createaccountController.js
+++ src/server/controllers/createaccountController.js
@@ -251,7 +251,7 @@ const isValidEmail = function (string) {
 
 const doPasswordFormatChecks = function (password, res) {
     // First we check password length
-    if (password.length < 6 || password.length > 30) return res.status(400).json({ 'message': 'Password must be between 6-30 characters long'});
+    if (password.length < 6) return res.status(400).json({ 'message': 'Password must be between 6-30 characters long'});
     if (!isValidPassword(password)) return res.status(400).json({ 'message': 'Password is in an incorrect format'});
     if (password.toLowerCase() === 'password') return res.status(400).json({ 'message': "Password must not be 'password'"});
     return true;
