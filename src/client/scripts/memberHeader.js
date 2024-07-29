c31658e40e11f6b74be7a31a5e39b5b33b18b4bd
Naviary <163621561+Naviary2@users.noreply.github.com>
2024-07-29 02:16:26 -0600

    No need for javascript to have theme-sensitive icon

--- src/client/scripts/memberHeader.js
+++ src/client/scripts/memberHeader.js
@@ -145,30 +145,3 @@ const validation = (function(){
     })
 
 })();
-
-favicon: { // This block auto detects device theme and adjusts the browser icon accordingly
-
-    const element_favicon = document.getElementById('favicon');
-
-    /**
-     * Switches the browser icon to match the given theme.
-     * @param {string} theme "dark"/"light"
-     */
-    function switchFavicon(theme) {
-        if (theme === 'dark') element_favicon.href = '/img/favicon-dark.png';
-        else favicon.href = '/img/favicon-light.png';
-    }
-      
-    if (!window.matchMedia) break favicon; // Don't create a theme-change event listener if matchMedia isn't supported.
-
-    // Initial theme detection
-    const prefersDarkScheme = window.matchMedia('(prefers-color-scheme: dark)').matches;
-    switchFavicon(prefersDarkScheme ? 'dark' : 'light');
-    
-    // Listen for theme changes
-    window.matchMedia('(prefers-color-scheme: dark)').addEventListener('change', event => {
-        const newTheme = event.matches ? 'dark' : 'light';
-        console.log(`Toggled ${newTheme} icon`);
-        switchFavicon(newTheme);
-    });
-}
