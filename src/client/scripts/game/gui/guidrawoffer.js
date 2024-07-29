1ee720d0d52e62ddc937bc8949464626981d96be
this-is-not-available <121245258+this-is-not-available@users.noreply.github.com>
2024-07-29 17:16:58 +0300

    snake case

--- src/client/scripts/game/gui/guidrawoffer.js
+++ src/client/scripts/game/gui/guidrawoffer.js
@@ -10,7 +10,7 @@ const guidrawoffer = (function(){
     // Variables
     // Draw Offer UI
     let isAcceptingDraw = false
-    const element_drawOfferUI = document.getElementById('drawOfferUI')
+    const element_draw_offer_ui = document.getElementById('draw_offer_ui')
     const element_acceptDraw = document.getElementById('acceptdraw')
     const element_declineDraw = document.getElementById('declinedraw')
 
@@ -24,7 +24,7 @@ const guidrawoffer = (function(){
 
     function openDrawOffer() {
         isAcceptingDraw = true;
-        style.revealElement(element_drawOfferUI)
+        style.revealElement(element_draw_offer_ui)
         // style.hideElement(element_whosturn)
         sound.playSound_drawOffer()
         initDrawOfferListeners()
@@ -32,7 +32,7 @@ const guidrawoffer = (function(){
 
     function closeDrawOffer() {
         isAcceptingDraw = false;
-        style.hideElement(element_drawOfferUI)
+        style.hideElement(element_draw_offer_ui)
         //style.revealElement(element_whosturn)
         closeDrawOfferListeners()
     }
