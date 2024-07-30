b0b4d518ab50bbb0e8bafb1a55bfb232c7ba7a86
jermOSS <175143807+jermOSS@users.noreply.github.com>
2024-07-30 21:38:40 +0200

    Integrate lightning CSS

--- build.mjs
+++ build.mjs
@@ -7,8 +7,14 @@
 
 import { readdir, cp as copy, rm as remove, readFile, writeFile } from "node:fs/promises";
 import swc from "@swc/core";
+import browserslist from 'browserslist';
+import { transform, browserslistToTargets } from 'lightningcss';
 import { injectScriptsIntoPlayEjs } from "./src/server/utility/HTMLScriptInjector.js"
 import { DEV_BUILD } from "./src/server/config/config.js";
+
+// Targetted browsers for CSS transpilation
+// Format: https://github.com/browserslist/browserslist?tab=readme-ov-file#query-composition
+const targets = browserslistToTargets(browserslist('defaults'));
 
 /**
  * 
@@ -54,7 +60,7 @@ if (DEV_BUILD){
     recursive: true,
     force: true,
     filter: filename => { 
-      return !/(\\|\/)scripts(\\|\/)/.test(filename) || /(\\|\/)game$/.test(filename) // make sure to create the scripts/game/folder
+      return (!/(\\|\/)scripts(\\|\/)/.test(filename) || /(\\|\/)game$/.test(filename)) && !/(\\|\/)css(\\|\/)/.test(filename)
     }
   });
 
@@ -93,4 +99,17 @@ if (DEV_BUILD){
   
   // overwrite play.ejs by injecting all needed scripts into it:
   await writeFile(`./dist/views/play.ejs`, injectScriptsIntoPlayEjs(), 'utf8');
+  
+  // Make a list of all css files
+  const cssFiles = await getExtFiles("./src/client/css", ".css");
+  for (const file of cssFiles) {
+    // Minify css files
+    let { code } = transform({
+      targets: targets,
+      code: Buffer.from(await readFile(`./src/client/css/${file}`, 'utf8')),
+      minify: true,
+    });
+    // Write into /dist
+    await writeFile(`./dist/css/${file}`, code, 'utf8');
+  }
 }
