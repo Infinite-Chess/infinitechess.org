d81008827cd6b76006ea05509a1bf18768e1dfb3
Naviary <163621561+Naviary2@users.noreply.github.com>
2024-07-31 13:56:48 -0600

    Comment correction

--- src/server/config/config.js
+++ src/server/config/config.js
@@ -18,7 +18,7 @@ const HOST_NAME = 'www.infinitechess.org';
  * The latest version of the game.
  * If the client is ever using an old version, we will tell them to hard-refresh.
  * 
- * THIS SHOULD ALWAYS MATCH protected-owner/scripts/game/main/DEV_VERSION
+ * THIS SHOULD ALWAYS MATCH protected-owner/scripts/game/main/GAME_VERSION
  */
 const GAME_VERSION = "1.4"
 
