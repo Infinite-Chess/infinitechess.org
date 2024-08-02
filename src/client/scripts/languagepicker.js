58b0d8dc064340464cd50f876f92a41980c6d18d
Naviary <163621561+Naviary2@users.noreply.github.com>
2024-08-02 00:52:40 -0600

    Reenabled removing of lng query param from url after loading

--- src/client/scripts/languagepicker.js
+++ src/client/scripts/languagepicker.js
@@ -51,17 +51,17 @@
   });
   
   /**  Modifies the URL in the browser's address bar to remove the "lng" query parameter without refreshing the page. */
-  // function removeLngQueryParam() {
-  //   // Create a URL object from the current window location
-  //   const url = new URL(window.location);
+  function removeLngQueryParam() {
+    // Create a URL object from the current window location
+    const url = new URL(window.location);
   
-  //   // Remove the "lng" query parameter
-  //   url.searchParams.delete('lng');
+    // Remove the "lng" query parameter
+    url.searchParams.delete('lng');
   
-  //   // Update the browser's URL without refreshing the page
-  //   window.history.replaceState({}, '', url);
-  // }
+    // Update the browser's URL without refreshing the page
+    window.history.replaceState({}, '', url);
+  }
  
-  // // Remove the "lng" param from the url bar when the DOM content is fully loaded
-  // document.addEventListener('DOMContentLoaded', removeLngQueryParam);
+  // Remove the "lng" param from the url bar when the DOM content is fully loaded
+  document.addEventListener('DOMContentLoaded', removeLngQueryParam);
 }
