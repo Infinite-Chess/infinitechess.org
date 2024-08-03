131e68b3979ed0601fb0f9327ccc648a7076aa90
Andreas Tsevas <tsevasa@gmail.com>
2024-08-03 02:20:07 +0200

    implemented random position generation

--- src/client/scripts/game/chess/checkmatepractice.js
+++ src/client/scripts/game/chess/checkmatepractice.js
@@ -1,6 +1,6 @@
 
 /**
- * This script handles checkmate practice generation
+ * This script handles checkmate practice logic
  */
 
 "use strict";
@@ -30,7 +30,7 @@ const checkmatepractice = (function() {
         "1K2N1B1B-1k",
         "1K1R1N1B-1k",
         "1K1R2N-1k",
-        "1K1R1GU-1k",
+        "2K1R-1k",
         "1K2AR-1k",
 
         // insane
@@ -39,31 +39,82 @@ const checkmatepractice = (function() {
         "1K3HA-1k",
     ];
 
+    /**
+     * This method generates a random starting position object for a given checkmate practice ID
+     * @param {string} checkmateID - a string containing the ID of the selected checkmate practice problem
+     * @returns a starting position object corresponding to that ID
+     */
     function generateCheckmateStartingPosition(checkmateID){
         // error if user somehow submitted invalid checkmate ID
         if (!validCheckmates.includes(checkmateID)) return console.error("User tried to play invalid checkmate practice.");
 
-        // the position to be generated
-        let startingPosition = {};
+        let startingPosition = {}; // the position to be generated
+        let blackpieceplaced = false; // monitors if a black piece has already been placed
+        let whitebishopparity = Math.floor(Math.random() * 2); // square color of first white bishop batch
         
         // read the elementID and convert it to a position
         const piecelist = checkmateID.match(/[0-9]+[a-zA-Z]+/g);
         for (let entry of piecelist) {
-            let amount = entry.match(/[0-9]+/)[0];
-            let piece = entry.match(/[a-zA-Z]+/)[0];
+            let amount = entry.match(/[0-9]+/)[0]; // number of pieces to be placed
+            let piece = entry.match(/[a-zA-Z]+/)[0]; // piecetype to be placed
             piece = formatconverter.ShortToLong_Piece(piece);
+
+            // place amount many pieces of type piece
             while (amount != 0) {
-                amount -= 1;
-                if (math.getPieceColorFromType(piece)) {
-                    // place randomly near origin
-                    // console.log(Math.random())
+                if (math.getPieceColorFromType(piece) === "white") {
+                    if (blackpieceplaced) return console.error("Must place all white pieces before placing black pieces.");
+
+                    // randomly generate white piece coordinates near origin in square from -5 to 5
+                    const x = Math.floor(Math.random() * 11) - 5;
+                    const y = Math.floor(Math.random() * 11) - 5;
+                    const key = math.getKeyFromCoords([x,y]);
+
+                    // check if square is occupied and white bishop parity is fulfilled
+                    if (!(key in startingPosition) && !(piece == "bishopsW" && (x + y)%2 != whitebishopparity)) {
+                        startingPosition[key] = piece;
+                        amount -= 1;
+                    }
                 } else {
-                    // place randomly at a distance
+                    // randomly generate black piece coordinates at a distance
+                    const x = Math.floor(Math.random() * 3) + 12;
+                    const y = Math.floor(Math.random() * 35) - 17;
+                    const key = math.getKeyFromCoords([x,y]);
+                    // check if square is occupied or potentially threatened
+                    if (!(key in startingPosition) && squareNotInSight(key, startingPosition)) {
+                        startingPosition[key] = piece;
+                        amount -= 1;
+                        blackpieceplaced = true;
+                    }
                 }
             }
+
+            // flip white bishop parity
+            whitebishopparity = 1 - whitebishopparity;
         }
 
-        return {"0,0": "pawnsW", "1,0": "queensW", "2,0": "queensW", "10,15": "kingsB"};
+        return startingPosition;
+    }
+
+    /**
+     * This method checks that the input square is not on the same row, column or diagonal as any key in the startingPosition object
+     * It also checks that it is not attacked by a knightrider
+     * @param {string} square - square of black piece
+     * @param {Object} startingPosition - startingPosition JSON containing all white pieces
+     * @returns true or false, depending on if the square is in sight or not
+     */
+    function squareNotInSight(square, startingPosition) {
+        const [sx,sy] = math.getCoordsFromKey(square);
+        for (let key in startingPosition){
+            const [x,y] = math.getCoordsFromKey(key);
+            if (x == sx || y == sy || Math.abs(sx - x) == Math.abs(sy - y)) return false;
+            if (startingPosition[key] === "knightridersW"){
+                if (Math.abs(sx-2*x) == Math.abs(sy - 2*y) || Math.abs(2*sx-x) == Math.abs(2*sy - y) ||
+                    Math.abs(sx-2*x) == Math.abs(2*sy - y) || Math.abs(2*sx-x) == Math.abs(sy - 2*y)) {
+                        return false;
+                    }
+            }
+        }
+        return true;
     }
 
     return Object.freeze({
