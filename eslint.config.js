f0287816ffdf92b0704ec63b9f78c71d6f71da50
Naviary <163621561+Naviary2@users.noreply.github.com>
2024-08-15 16:40:39 -0600

    Added custom eslint rulesets

--- eslint.config.js
+++ eslint.config.js
@@ -1,12 +1,38 @@
+/* eslint-disable indent */
 const globals = require("globals");
 const pluginJs = require("@eslint/js");
 
 module.exports = [
-  pluginJs.configs.recommended, // Overwrites "rules" below
+  pluginJs.configs.recommended,
   {
     rules: { // Overrides the preset defined by "pluginJs.configs.recommended" above
-      'no-undef': 'error', // Undefined variables
-      'no-unused-vars': 'warn', // Unused variables
+      'no-undef': 'error', // Undefined variables not allowed
+      'no-unused-vars': 'warn', // Unused variables give a warning
+      'semi': ['error', 'always'], // Enforces semicolons be present at the end of every line.
+      'semi-spacing': ['error', { // Enforces semicolons have a space after them if they are proceeded by other statements.
+        before: false,
+        after: true,
+      }],
+      'keyword-spacing': ['error', { // Requires a space be after if, else, for, and while's.
+        before: true,
+        after: true,
+      }],
+      "space-before-function-paren": ["error", "never"], // Enforces there be NO space between function DECLARATIONS and ()
+      "space-before-blocks": ["error", "always"], // Enforces there be a space between function parameters and the {} block
+      "arrow-spacing": ["error", { "before": true, "after": true }], // Requires a space before and after "=>" in arrow functions
+      "func-call-spacing": ["error", "never"], // Enforces there be NO space between function CALLS and ()
+      "space-infix-ops": ["error", { "int32Hint": false }], // Enforces a space around infix operators, like "=" in assignments
+      "no-eval": "error", // Disallows use of `eval()`, as it can lead to security vulnerabilities and performance issues.
+      'indent': ['error', 4, { // 4 spaces per indentation
+        'SwitchCase': 1 // Enforce switch statements to have indentation (they don't by default)
+      }],
+      "prefer-const": "error", // "let" variables that are never redeclared must be declared as "const"
+      "no-var": "error", // Disallows delcaring variables with "var", as they are function-scoped (not block), so hoisting is very confusing.
+      "max-depth": ["warn", 4], // Maximum number of nested blocks allowed.
+      "no-multi-spaces": "error", // Disallows multiple spaces that isn't indentation.
+      "eqeqeq": ["error", "always"], // Disallows "!=" and "==" to remove type coercion bugs. Use "!==" and "===" instead.
+      // "max-lines": ["warn", 500] // Can choose to enable to place a cap on how big files can be, in lines.
+      // "complexity": ["warn", { "max": 10 }] // Can choose to enable to cap the complexity, or number of independant paths, which can lead to methods.
     },
     languageOptions: {
       sourceType: "module", // Can also be "commonjs", but "import" and "export" statements will give an eslint error
