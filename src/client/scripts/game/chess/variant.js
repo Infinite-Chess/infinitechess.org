6aba90bb8e01e818103cb8056e26e731f5c4b187
Naviary <163621561+Naviary2@users.noreply.github.com>
2024-08-14 21:51:36 -0600

    Updated timestamp for new Knighted Chess position

--- src/client/scripts/game/chess/variant.js
+++ src/client/scripts/game/chess/variant.js
@@ -402,9 +402,9 @@ const variant = (function() {
      */
     function getPositionStringOfKnightedChess(UTCDate, UTCTime) {
         const UTCTimeStamp = UTCDate ? math.convertUTCDateUTCTimeToTimeStamp(UTCDate, UTCTime) : Date.now();
-        // UTC timestamp for Jul 21, 2024, 9:33 PM
+        // UTC timestamp for Aug 1, 2024, 12:00AM
         // Original, oldest version. NO knightrider
-        if (UTCTimeStamp < 1721619205980) return 'P1,2+|P2,2+|P3,2+|P4,2+|P5,2+|P6,2+|P7,2+|P8,2+|p1,7+|p2,7+|p3,7+|P0,1+|P1,0+|P2,0+|P3,0+|P6,0+|P7,0+|P8,0+|P9,1+|p4,7+|p5,7+|p6,7+|p7,7+|p8,7+|p0,8+|p1,9+|p2,9+|p3,9+|p6,9+|p7,9+|p8,9+|p9,8+|CH1,1+|CH8,1+|ch1,8+|ch8,8+|N2,1|N7,1|n2,8|n7,8|AR3,1|AR6,1|ar3,8|ar6,8|AM4,1|am4,8|RC5,1+|rc5,8+'
+        if (UTCTimeStamp < 1722470400000) return 'P1,2+|P2,2+|P3,2+|P4,2+|P5,2+|P6,2+|P7,2+|P8,2+|p1,7+|p2,7+|p3,7+|P0,1+|P1,0+|P2,0+|P3,0+|P6,0+|P7,0+|P8,0+|P9,1+|p4,7+|p5,7+|p6,7+|p7,7+|p8,7+|p0,8+|p1,9+|p2,9+|p3,9+|p6,9+|p7,9+|p8,9+|p9,8+|CH1,1+|CH8,1+|ch1,8+|ch8,8+|N2,1|N7,1|n2,8|n7,8|AR3,1|AR6,1|ar3,8|ar6,8|AM4,1|am4,8|RC5,1+|rc5,8+'
         // Latest version, with knightrider
         else return 'P1,2+|P2,2+|P3,2+|P4,2+|P5,2+|P6,2+|P7,2+|P8,2+|p1,7+|p2,7+|p3,7+|P0,1+|P1,0+|P2,0+|P3,0+|P6,0+|P7,0+|P8,0+|P9,1+|p4,7+|p5,7+|p6,7+|p7,7+|p8,7+|p0,8+|p1,9+|p2,9+|p3,9+|p6,9+|p7,9+|p8,9+|p9,8+|CH1,1+|CH8,1+|ch1,8+|ch8,8+|NR2,1|NR7,1|nr2,8|nr7,8|AR3,1|AR6,1|ar3,8|ar6,8|AM4,1|am4,8|RC5,1+|rc5,8+'
     }
