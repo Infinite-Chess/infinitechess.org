fdd0dce3040af79ffaa369f8e9f04b748e3a6140
Naviary <163621561+Naviary2@users.noreply.github.com>
2024-08-26 00:51:40 -0600

    Some polishing (unfinished, it will be broken)

--- src/server/game/variant1.js
+++ src/server/game/variant1.js
@@ -1,8 +1,8 @@
 
-import { formatconverter1 } from './formatconverter1.js'
-import { variantomega1 } from './variantomega1.js'
-import { math1 } from './math1.js'
-import { pieces1 } from './pieces1.js'
+import formatconverter1 from './formatconverter1.js';
+import variantomega1 from './variantomega1.js';
+import math1 from './math1.js';
+import pieces1 from './pieces1.js';
 
 // This script stores our variants,
 // and prepares them when a game is generated
@@ -263,7 +263,7 @@ const variant1 = (function() {
             winConditions: modifications.winConditions || getDefaultWinConditions(),
             moveRule: modifications.moveRule || 100,
             turnOrder: modifications.turnOrder || getDefaultTurnOrder(),
-        }
+        };
         if (modifications.slideLimit != null) gameRules.slideLimit = modifications.slideLimit;
         if (modifications.moveRule === null) delete gameRules.moveRule;
         return gameRules;
@@ -915,4 +915,4 @@ const variant1 = (function() {
 
 })();
 
-export { variant1 };
+export default variant1;
