945b88f69dab171c1901aa9585fe862510a80d23
Naviary2 <infinitechess.org@gmail.com>
2024-10-30 15:34:26 -0600

    New header now works on member page

--- src/client/scripts/member.js
+++ src/client/scripts/member.js
@@ -1,12 +1,15 @@
 
-// This script does what the memberHeader script does, except it also detects if we're on the same page as who we're logged in as.
-
-let token;
-
-const element_loginLink = document.getElementById('loginlink');
-const element_loginText = document.getElementById('logintext');
-const element_createaccountLink = document.getElementById('createaccountlink');
-const element_createaccountText = document.getElementById('createaccounttext');
+/*
+ * This script:
+ * 
+ * * Fetches the data of the member's page we're viewing
+ * so we can display that info.
+ * 
+ * * Dynamically adjusts the font-size of the username.
+ * Resends confirmation emails upon clicking the button.
+ * 
+ * * Deletes account when button clicked and password entered.
+ */
 
 
 const element_verifyErrorElement = document.getElementById('verifyerror');
@@ -42,55 +45,8 @@ function getLastSegmentOfURL() {
 	return segments[segments.length - 1] || segments[segments.length - 2]; // Handle situation if trailing '/' is present
 }
 
-refreshAndUpdateNav();
-
-function refreshAndUpdateNav() {
-	// Fetch an access token by refreshing
-	let OK = false;
-	fetch('/refresh')
-		.then((response) => {
-			if (response.ok) OK = true;
-			return response.json();
-		})
-		.then((result) => {
-			if (OK) { // Refresh token (from cookie) accepted! Receiving new access token + member name
-				console.log("Logged in");
-				// token = result.accessToken;
-				token = getCookieValue('token'); // Cookie expires in 60s
-            
-				loadMemberData(result.member.toLowerCase());
-
-				// Change navigation links...
-				element_loginLink.setAttribute('href', `/member/${result.member.toLowerCase()}`);
-				// element_loginText.textContent = result.member;
-				element_loginText.textContent = translations["js-profile"];
-
-				element_createaccountLink.setAttribute('href', '/logout');
-				element_createaccountText.textContent = translations["js-logout"];
-
-			} else { // Unauthorized, don't change any navigation links
-				console.log(result.message);
-				loadMemberData();
-			}
-		});
-}
-
-/**
- * Fetches data from a given endpoint after removing any query parameters from the URL.
- * 
- * @param {string} member - The member identifier to include in the URL.
- * @param {Object} config - The configuration object for the fetch request.
- * @returns {Promise<Response>} - The fetch response promise.
- */
-function removeQueryParamsFromLink(link) {
-	const url = new URL(link, window.location.origin);
-	// Remove query parameters
-	url.search = '';
-	return url.toString();
-}
-
-function loadMemberData(loggedInAs) {
-	const config = { // Send with our access token
+(function loadMemberData() {
+	const config = {
 		method: 'GET',
 		headers: {
 			'Content-Type': 'application/json'
@@ -98,7 +54,7 @@ function loadMemberData(loggedInAs) {
 		credentials: 'same-origin', // Allows cookie to be set from this request
 	};
 	// If we're logged in, include authorization
-	if (token) config.headers.Authorization = `Bearer ${token}`;
+	// if (token) config.headers.Authorization = `Bearer ${token}`; // DON'T NEED, server reads our refresh token cookie
 
 	fetch(`/member/${member}/data`, config)
 		.then((response) => {
@@ -106,7 +62,7 @@ function loadMemberData(loggedInAs) {
 			if (response.status === 500) window.location = '/500';
 			return response.json();
 		})
-		.then((result) => { // result.verified = true/false
+		.then(async(result) => { // result.verified = true/false
 			console.log(result); // { elo, joined, seen, username, email, verified }
 
 			// Change on-screen data of the member
@@ -118,36 +74,36 @@ function loadMemberData(loggedInAs) {
 			const seenElement = document.getElementById('seen');
 			seenElement.textContent = result.seen;
 
+			await header.waitUntilInitialRequestBack(); // Wait until we know if we are logged in or not
+			const loggedInAs = header.getOurUsername();
+
 			// Is it our own profile?
 			if (loggedInAs === result.username.toLowerCase()) {
 				isOurProfile = true;
 
-				// Grey background our profile nav link.
-				element_loginText.className = 'currPage';
-
 				// If this account has not yet confirmed their email, make that error visible.
 				// Our json will not contain this parameter if we aren't logged in.
-				if (result.verified === true) revealElement(element_verifyConfirmElement); // They just confirmed, tell them it was a success!
-				else if (result.verified === false) revealElement(element_verifyErrorElement);
+				if (result.verified === true) element_verifyConfirmElement.classList.remove('hidden'); // They just confirmed, tell them it was a success!
+				else if (result.verified === false) element_verifyErrorElement.classList.remove('hidden');
 
 				// Display email
-				revealElement(element_showAccountInfo);
+				element_showAccountInfo.classList.remove('hidden');
 				// Display remove button
-				revealElement(element_deleteAccount);
+				element_deleteAccount.classList.remove('hidden');
 				element_deleteAccount.addEventListener("click", () => removeAccount(true));
 				// revealElement(element_accountInfo)
-				revealElement(element_change);
+				element_change.classList.remove('hidden');
 				element_email.textContent = result.email;
 			}
 
 			// Change username text size depending on character count
 			recalcUsernameSize();
 		});
-}
+})();
 
-function showAccountInfo() {
-	hideElement(element_showAccountInfo); // Button
-	revealElement(element_accountInfo);
+function showAccountInfo() { // Called from inside the html
+	element_showAccountInfo.classList.add("hidden");
+	element_accountInfo.classList.remove("hidden");
 }
 
 async function removeAccount(confirmation) {
@@ -156,11 +112,11 @@ async function removeAccount(confirmation) {
 		const cancelWasPressed = password === null;
 		if (cancelWasPressed) return; // Don't delete account
 
-		const config = { // Send with our access token
+		const config = {
 			method: 'DELETE',
 			headers: {
 				'Content-Type': 'application/json',
-				'Authorization': `Bearer ${token}`
+				// 'Authorization': `Bearer ${token}` // DON'T NEED, server reads our refresh token cookie
 			},
 			body: JSON.stringify({ password }),
 			credentials: 'same-origin', // Allows cookie to be set from this request
@@ -186,7 +142,7 @@ function resendConfirmEmail() {
 		method: 'GET',
 		headers: {
 			'Content-Type': 'application/json',
-			'Authorization': `Bearer ${token}`
+			// 'Authorization': `Bearer ${token}` // DON'T NEED, server reads our refresh token cookie
 		},
 		credentials: 'same-origin', // Allows cookie to be set from this request
 	};
@@ -210,26 +166,6 @@ function recalcUsernameSize() {
 	const cap = 50;
 	if (fontSize > cap) fontSize = cap;
 	element_memberName.style["font-size"] = `${fontSize}px`;
-}
-
-function getCookieValue(cookieName) {
-	const cookieArray = document.cookie.split("; ");
-    
-	for (let i = 0; i < cookieArray.length; i++) {
-		const cookiePair = cookieArray[i].split("=");
-  
-		if (cookiePair[0] === cookieName) {
-			return cookiePair[1];
-		}
-	}
-}
-
-function hideElement(element) {
-	element.classList.add("hidden");
-}
-
-function revealElement(element) {
-	element.classList.remove("hidden");
 }
 
 window.addEventListener("resize", recalcUsernameSize);
