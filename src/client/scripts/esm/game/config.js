2221344548172f1b0ad7e92e718099ad9d0e53e6
Naviary2 <infinitechess.org@gmail.com>
2024-11-14 15:07:44 -0700

    Change version number of the game to 1.5

--- src/client/scripts/esm/game/config.js
+++ src/client/scripts/esm/game/config.js
@@ -8,7 +8,7 @@ import docutil from "../util/docutil.js";
  * 
  * THIS SHOULD ALWAYS MATCH src/server/config/config.GAME_VERSION
  */
-const GAME_VERSION = "1.4.4"; // The current version of the game
+const GAME_VERSION = "1.5"; // The current version of the game
 
 /** Video mode disables the rendering of some items, making making recordings more immersive. */
 const VIDEO_MODE = false;
