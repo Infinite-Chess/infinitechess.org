513e631cc55a90ced7f87a5fa95cd1ec21f7cffe
Naviary2 <infinitechess.org@gmail.com>
2024-11-17 14:37:38 -0700

    No need to copy the standard position to a new object

--- src/client/scripts/esm/chess/variants/fivedimensionalgenerator.js
+++ src/client/scripts/esm/chess/variants/fivedimensionalgenerator.js
@@ -15,17 +15,16 @@ function genPositionOfFiveDimensional() {
 	const standardPosStr = 'P1,2+|P2,2+|P3,2+|P4,2+|P5,2+|P6,2+|P7,2+|P8,2+|p1,7+|p2,7+|p3,7+|p4,7+|p5,7+|p6,7+|p7,7+|p8,7+|R1,1+|R8,1+|r1,8+|r8,8+|N2,1|N7,1|n2,8|n7,8|B3,1|B6,1|b3,8|b6,8|Q4,1|q4,8|K5,1+|k5,8+';
 
 	const standardPos = formatconverter.ShortToLong_Format(standardPosStr).startingPosition;
-	const result = { ...standardPos };
 
 	for (let i = -(BOARD_SPACING * Math.floor(BOARDS_X / 2) + 1); i <= BOARD_SPACING * (Math.floor(BOARDS_X / 2) + 1); i++) {
 		for (let j = -(10 * Math.floor(BOARDS_Y / 2) + 1); j <= 10 * (Math.floor(BOARDS_Y / 2) + 1); j++) {
 			if ((i % 10 === -1 || i % 10 === 0 || i % 10 === 9) || (j % 10 === -1 || j % 10 === 0 || j % 10 === 9)) {
-				result[coordutil.getKeyFromCoords([i, j])] = 'voidsN';
+				standardPos[coordutil.getKeyFromCoords([i, j])] = 'voidsN';
 			}
 		}
 	}
 
-	return result;
+	return standardPos;
 }
 
 function genMovesetOfFiveDimensional() {
