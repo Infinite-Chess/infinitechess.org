a90b07207666ea41529afc9fe3df91f5eab0a07f
this-is-not-available <121245258+this-is-not-available@users.noreply.github.com>
2024-12-23 14:37:43 +0200

    Some code to display them
    Not perfect

---
+++ src/client/scripts/esm/views/index.js
@@ -0,0 +1,27 @@
+const githubContributors = document.getElementsByClassName("github-container")[0];
+
+const res = fetch("/api/contributors");
+res.then(function(contributors) {
+	contributors.json().then(function(json) {
+		console.log(json);
+		json.forEach(contributor => {
+			const link = document.createElement('a');
+			link.id = 'github-template';
+			link.href = contributor.linkUrl;
+
+			const img = document.createElement('img');
+			img.src = contributor.iconUrl;
+
+			const div = document.createElement('div');
+			div.className = 'github-stats';
+
+			const paragraph = document.createElement('p');
+			paragraph.textContent = `${contributor.contributionCount} contributions`;
+
+			div.appendChild(paragraph);
+			link.appendChild(img);
+			link.appendChild(div);
+			githubContributors.appendChild(link);
+		});
+	});
+});
