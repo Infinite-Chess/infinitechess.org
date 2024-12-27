2929322ae21548a9e70e9a3ff387fcacb1504b73
this-is-not-available <121245258+this-is-not-available@users.noreply.github.com>
2024-12-27 10:22:21 +0200

    Cleanup script
    reorder, change variable names, remove console.log and add error handling

--- src/client/scripts/esm/views/index.js
+++ src/client/scripts/esm/views/index.js
@@ -3,25 +3,26 @@ const githubContributors = document.getElementsByClassName("github-container")[0
 const res = fetch("/api/contributors");
 res.then(function(contributors) {
 	contributors.json().then(function(json) {
-		console.log(json);
 		json.forEach(contributor => {
 			const link = document.createElement('a');
-			link.href = contributor.linkUrl;
-
-			const img = document.createElement('img');
-			img.src = contributor.iconUrl;
-
-			const div = document.createElement('div');
-			div.className = 'github-stats';
-
+			const iconImg = document.createElement('img');
+			const githubStatsContainer = document.createElement('div');
+			githubStatsContainer.className = 'github-stats';
 			const paragraph = document.createElement('p');
+			
 			const contributionText = `${translations.contribution_count[0]} ${contributor.contributionCount} ${translations.contribution_count[1]}`;
+			link.href = contributor.linkUrl;
+			iconImg.src = contributor.iconUrl;
 			paragraph.innerText = `${contributor.name}\n${contributionText}`;
 
-			div.appendChild(paragraph);
-			link.appendChild(img);
-			link.appendChild(div);
+			githubStatsContainer.appendChild(paragraph);
+			link.appendChild(iconImg);
+			link.appendChild(githubStatsContainer);
 			githubContributors.appendChild(link);
 		});
 	});
+});
+res.catch(function(reason) {
+	console.warn("COULD NOT LOAD CONTRIBUTOR LIST");
+	console.error(reason);
 });
