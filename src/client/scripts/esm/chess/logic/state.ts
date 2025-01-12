4e3e92f343d3cef3067d6da38e19da3d93a9fa50
Naviary2 <infinitechess.org@gmail.com>
2025-01-11 18:31:01 -0700

    Added documentation to state.ts

--- src/client/scripts/esm/chess/logic/state.ts
+++ src/client/scripts/esm/chess/logic/state.ts
@@ -1,52 +1,101 @@
-// @ts-ignore
-import coordutil from "../util/coordutil.js";
+
 
 // @ts-ignore
 import type { Move } from "../util/moveutil.js";
 // @ts-ignore
 import type { gamefile } from "./gamefile.js";
 
-/** Contains the local and global gamefile state changes from a Move */
+
+import coordutil from "../util/coordutil.js";
+
+
+// Type Definitions ------------------------------------------------------------------------------------
+
+
+/**
+ * Contains the statechanges for the turn before and after a move is made
+ * 
+ * Local state change examples: (check, attackers)
+ * Global state change examples: (enpassant, specialrights, overule state, running check counter)
+ */
 interface MoveState {
-	// Local statechanges are unique to the position you're viewing, and are ALWAYS applied.
 	local: Array<StateChange>,
-	// Global statechanges are a property of the game as a whole, not unique to the move,
-	// and are not applied when VIEWING a move.
-	// However, they are applied only when we make a new move, or rewind a simulated one.
 	global: Array<StateChange>,
 }
 
-/** A state change, local or global, that contains enough information to set the gamefile's
- * property whether the move is being rewound or replayed. */
+/**
+ * A state change, local or global, that contains enough information to set the gamefile's
+ * property whether the move is being rewound or replayed.
+ */
 type StateChange = {
-	// All valid state change types
-	type: StateNames,
-	// The gamefile's property of this type BEFORE this move was made, used to restore them when the move is rewinded.
+	/** The type of state this {@link StateChange} is */
+	type: StateType,
+	/* The gamefile's property of this type BEFORE this move was made, used to restore them when the move is rewinded. */
 	current: any,
-	// The gamefile's property of this type AFTER this move was made, used to restore them when the move is replayed.
+	/* The gamefile's property of this type AFTER this move was made, used to restore them when the move is replayed. */
 	future: any,
-	// Additional state change properties, shared across both 'current' and 'future'.
-	// For example, the coordinates of a specialrights state change.
+	/**
+	 * Additional state change properties, shared across both 'current' and 'future'.
+	 * For example, the coordinates of a specialrights state change.
+	 */
 	[changeProperty: string]: any
 }
 
+/** A list of all state types there are. */
 const stateTypes = {
-	local: ['check', 'attackers', 'enpassant', 'specialrights'],
-	global: ['moverulestate']
+	/** Local statechanges are unique to the move you're viewing, and are always applied. */
+	local: ['check', 'attackers'],
+	/**
+	 * Global statechanges are a property of the game as a whole, not unique to the move,
+	 * and are not applied when VIEWING a move.
+	 * However, they are applied only when we make a new move, or rewind a simulated one.
+	 */
+	global: ['enpassant', 'specialrights', 'moverulestate']
 };
 
-type StateNames = 'specialrights' | 'check' | 'attackers' | 'enpassant' | 'moverulestate';
+/** A type of a {@link StateChange} */
+type StateType = 'specialrights' | 'check' | 'attackers' | 'enpassant' | 'moverulestate';
 
+
+// Functions ---------------------------------------------------------------------------------------------
+
+
+/** Initializes the `state` property of the {@link Move} object */
 function initMoveStates(move: Move) {
-	move.state = {local: [], global: []};
+	move.state = { local: [], global: [] };
 }
 
-/** Applies the state of a move to the gamefile. */
-function applyState(gamefile: gamefile, state: StateChange, forward: boolean) {
+/**
+ * Applies all the StateChanges of a Move, in order, to the gamefile,
+ * whether forward or backward, local or global.
+ */
+function applyMove(
+	gamefile: gamefile,
+	move: Move,
+	/** Whether we're playing this move forward or backward. */
+	forward: boolean,
+	/**
+	 * Specify `globalChange` as true if you are making a physical move in the game,
+	 * or rewinding a simulated move.
+	 * All other situations, such as rewinding and forwarding the game, should only
+	 * be local, so `globalChange` should be false.
+	 */
+	{ globalChange = false } = {}
+) {
+	for (const change of move.state.local) {
+		applyState(gamefile, change, forward);
+	}
+	if (!globalChange) return;
+	for (const change of move.state.global) {
+		applyState(gamefile, change, forward);
+	}
+}
 
+/** Applies the state of a move to the gamefile, whether forward or backward. */
+function applyState(gamefile: gamefile, state: StateChange, forward: boolean) {
 	const newValue = forward ? state.future : state.current;
 	switch (state.type) {
-		case 'specialrights': {
+		case 'specialrights': { // Use brackets to prevent these variables from being hoisted
 			const coordsKey = coordutil.getKeyFromCoords(state['coords']);
 			if (newValue === undefined) delete gamefile.specialRights[coordsKey];
 			else gamefile.specialRights[coordsKey] = newValue;	
@@ -67,21 +116,27 @@ function applyState(gamefile: gamefile, state: StateChange, forward: boolean) {
 }
 
 /**
- * 
- * @param gamefile 
- * @param stateType 
- * @param current 
- * @param future 
- * @param param4 An object containing additional state change properties, shared across both 'current' and 'future'. For example, the coordinates of a specialrights state change.
- * @param gamefileToSet The gamefile to set state, if not supplied it will just queue it in move
-*/
+ * Creates a StateChange, queueing it by adding it to the Move,
+ * and optionally applying the change immediately to the gamefile.
+ */
 function createState(
 	move: Move,
-	stateType: StateNames,
+	/** The type of state this StateChange is for. */
+	stateType: StateType,
+	/** The current value of this gamefile's property, BEFORE making the move. */
 	current: any,
+	/** The value of this gamefile's property, AFTER making the move. */
 	future: any,
+	/** An object containing additional state change properties, shared across both 'current' and 'future'. For example, the coordinates of a specialrights state change. */
 	changeProperties: { [changeProperty: string]: any } = {},
-	gamefileToSet: gamefile | undefined = undefined
+	/**
+	 * ONLY PROVIDE IF YOU want to immediately apply the state!!!
+	 * Do this for the `check` and `attackers` states, as they can only be calculated
+	 * AFTER making the move, so they need to be applied immediately.
+	 * 
+	 * If not supplied, we will only queue the StateChange in the move.
+	 */
+	gamefileToSet?: gamefile
 ) {
 	const newStateChange = { type: stateType, current, future, ...changeProperties };
 	const targetStateChangeList = stateTypes.global.includes(stateType) ? move.state.global : move.state.local;
@@ -90,15 +145,7 @@ function createState(
 	if (gamefileToSet !== undefined) applyState(gamefileToSet, newStateChange, true);
 }
 
-function applyMove(gamefile: gamefile, move: Move, forward: boolean, { globalChange = false } = {}) {
-	for (const change of move.state.local) {
-		applyState(gamefile, change, forward);
-	}
-	if (!globalChange) return;
-	for (const change of move.state.global) {
-		applyState(gamefile, change, forward);
-	}
-}
+
 
 export type {
 	MoveState
