f0c6c54d3d0326a3408d4fb3b6af520a3bcfaf6e
Naviary2 <infinitechess.org@gmail.com>
2025-01-19 16:31:01 -0700

    Added variant Confined Classical

--- src/client/scripts/esm/chess/variants/variant.ts
+++ src/client/scripts/esm/chess/variants/variant.ts
@@ -178,6 +178,10 @@ const variantDictionary: { [variantName: string]: Variant } = {
 	// 	positionString: positionStringOfCoaIP,
 	// 	gameruleModifications: { winConditions: KOTHWinConditions, promotionsAllowed: coaIPPromotionsAllowed }
 	// },
+	Confined_Classical: {
+		positionString: 'P1,2+|P2,2+|P3,2+|P4,2+|P5,2+|P6,2+|P7,2+|P8,2+|p1,7+|p2,7+|p3,7+|p4,7+|p5,7+|p6,7+|p7,7+|p8,7+|R1,1+|R8,1+|r1,8+|r8,8+|N2,1|N7,1|n2,8|n7,8|B3,1|B6,1|b3,8|b6,8|Q4,1|q4,8|K5,1+|k5,8+|ob0,0|ob0,1|ob0,2|ob0,7|ob0,8|ob0,9|ob9,0|ob9,1|ob9,2|ob9,7|ob9,8|ob9,9|ob1,0|ob2,0|ob3,0|ob4,0|ob5,0|ob6,0|ob7,0|ob8,0|ob1,9|ob2,9|ob3,9|ob4,9|ob5,9|ob6,9|ob7,9|ob8,9',
+		gameruleModifications: { promotionsAllowed: defaultPromotionsAllowed }
+	},
 	Classical_Plus: {
 		positionString: 'p1,9+|p2,9+|p3,9+|p6,9+|p7,9+|p8,9+|p0,8+|r1,8+|n2,8|b3,8|q4,8|k5,8+|b6,8|n7,8|r8,8+|p9,8+|p1,7+|p2,7+|p3,7+|p4,7+|p5,7+|p6,7+|p7,7+|p8,7+|p3,5+|p6,5+|P3,4+|P6,4+|P1,2+|P2,2+|P3,2+|P4,2+|P5,2+|P6,2+|P7,2+|P8,2+|P0,1+|R1,1+|N2,1|B3,1|Q4,1|K5,1+|B6,1|N7,1|R8,1+|P9,1+|P1,0+|P2,0+|P3,0+|P6,0+|P7,0+|P8,0+',
 		gameruleModifications: { promotionsAllowed: defaultPromotionsAllowed }
