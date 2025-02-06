301915b4726bf679b0feef08369b704a2bc694e0
xa31er <condexabier@gmail.com>
2025-02-06 12:26:00 +0100

    Cleaned up unnecesary code

--- src/server/controllers/emailValidator.js
+++ src/server/controllers/emailValidator.js
@@ -4,11 +4,15 @@
 
 import emailValidator from 'node-email-verifier';
 
+/**
+ * Checks an adress's MX records yo see if it can recieve mail
+ * 
+ * @param {string} email the email to verify
+ * @returns {boolean | undefined} returns wether the email can recieve messages (undefined if error)
+ */
 async function canEmailRecieveMail(email) {
-	console.log(`Checking "${email}" MX records to see if we cand send mail to it...`);
 	try {
 		const isValid = await emailValidator(email, { checkMx: true });
-		console.log("Can we? ", isValid);
 		return isValid;
 	} catch (error) {
 		console.error('Error validating email with MX checking:', error);
