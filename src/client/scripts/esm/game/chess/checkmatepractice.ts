d46bad0b4cbbd06b5b6720948b74d823dfccd73e
Andreas Tsevas <tsevasa@gmail.com>
2025-02-18 10:32:53 +0100

    post-merge fix

--- src/client/scripts/esm/game/chess/checkmatepractice.ts
+++ src/client/scripts/esm/game/chess/checkmatepractice.ts
@@ -25,42 +25,42 @@ import formatconverter from '../../chess/logic/formatconverter.js';
 
 // Variables ----------------------------------------------------------------------------
 
-
-const validCheckmates: string[] = [
-	// easy
-	"2Q-1k",
-	"3R-1k",
-	"3B3B-1k",
-	"1K2B2B-1k",
-	"1K2R-1k",
-	"2CH-1k",
-	"1Q1CH-1k",
-	"1K1AM-1k",
-
-	// medium
-	"1K1R1B1B-1k",
-	"1K1Q1B-1k",
-	"1K1Q1N-1k",
-	"1Q1B1B-1k",
-	"1Q2N-1k",
-	"1K1AR1R-1k",
-	"1K2AR-1k",
-	"2AM-1rc",
-
-	// hard
-	"1K1N2B1B-1k",
-	"1K2N1B1B-1k",
-	"1K1R1N1B-1k",
-	"2R1N1P-1k",
-	"1K1R2N-1k",
-	"2K1R-1k",
-	"1K2N6B-1k",
-
-	// insane
-	"1K1Q1P-1k",
-	"1K3NR-1k",
-	"1K3HA-1k",
-];
+const validCheckmates = {
+	easy: [
+		"2Q-1k",
+		"3R-1k",
+		"3B3B-1k",
+		"1K2B2B-1k",
+		"1K2R-1k",
+		"2CH-1k",
+		"1Q1CH-1k",
+		"1K1AM-1k"
+	],
+	medium: [
+		"1K1R1B1B-1k",
+		"1K1Q1B-1k",
+		"1K1Q1N-1k",
+		"1Q1B1B-1k",
+		"1Q2N-1k",
+		"1K1AR1R-1k",
+		"1K2AR-1k",
+		"2AM-1rc"
+	],
+	hard: [
+		"1K1N2B1B-1k",
+		"1K2N1B1B-1k",
+		"1K1R1N1B-1k",
+		"2R1N1P-1k",
+		"1K1R2N-1k",
+		"2K1R-1k",
+		"1K2N6B-1k"
+	],
+	insane: [
+		"1K1Q1P-1k",
+		"1K3NR-1k",
+		"1K3HA-1k"
+	]
+};
 
 const nameOfCompletedCheckmatesInStorage: string = 'checkmatePracticeCompletion';
 /**
