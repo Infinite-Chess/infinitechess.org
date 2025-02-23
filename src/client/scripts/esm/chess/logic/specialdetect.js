f556412c5cb75d5db7ec9353ad04ab8c996f5603
Naviary2 <infinitechess.org@gmail.com>
2025-02-23 10:53:14 -0700

    Prevent illegal enpassant capture edge cases when using EDIT mode

--- src/client/scripts/esm/chess/logic/specialdetect.js
+++ src/client/scripts/esm/chess/logic/specialdetect.js
@@ -202,6 +202,8 @@ function getEnPassantGamefileProperty(moveStartCoords, moveEndCoords) {
 function addPossibleEnPassant(gamefile, individualMoves, coords, color) {
 	if (gamefile.enpassant === undefined) return; // No enpassant flag on the game, no enpassant possible
 	if (color !== gamefile.whosTurn) return; // Not our turn (the only color who can legally capture enpassant is whos turn it is). If it IS our turn, this also guarantees the captured pawn will be an enemy pawn.
+	const enpassantCapturedPawn = gamefileutility.getPieceTypeAtCoords(gamefile, gamefile.enpassant.pawn);
+	if (colorutil.getPieceColorFromType(enpassantCapturedPawn) === color) return; // The captured pawn is not an enemy pawn. THIS IS ONLY EVER NEEDED if we can move opponent pieces on our turn, which is the case in EDIT MODE.
 
 	const xDifference = gamefile.enpassant.square[0] - coords[0];
 	if (Math.abs(xDifference) !== 1) return; // Not immediately left or right of us
