014688a85b3ede29ddd296827c0a52ccc04609bc
Naviary2 <infinitechess.org@gmail.com>
2025-02-25 13:52:11 -0700

    Removed the unnecessary nesting levels in the client event dispatcher

--- src/client/scripts/esm/util/clientEventDispatcher.ts
+++ src/client/scripts/esm/util/clientEventDispatcher.ts
@@ -5,37 +5,41 @@
  * NOTHING will happen if it is imported, or its methods are called, in the Node.js environment.
  */
 
-const clientEventDispatcher = (() => {
-	const isBrowser = typeof window !== 'undefined' && typeof window.dispatchEvent === 'function';
-	const target: Window = isBrowser ? window : null!;
 
-	return {
-		/**
-		 * Dispatches an event with the given name.
-		 * @param eventName The name of the event to dispatch.
-		 */
-		dispatch(eventName: string): void {
-			if (isBrowser) target.dispatchEvent(new Event(eventName));
-		},
+const isBrowser = typeof window !== 'undefined' && typeof window.dispatchEvent === 'function';
+const target: Window = isBrowser ? window : null!;
 
-		/**
-		 * Listens for an event with the given name.
-		 * @param eventName The name of the event to listen for.
-		 * @param callback The callback function to invoke when the event occurs.
-		 */
-		listen(eventName: string, callback: () => void): void {
-			if (isBrowser) target.addEventListener(eventName, callback as EventListener);
-		},
 
-		/**
-		 * Removes a previously added event listener.
-		 * @param eventName The name of the event.
-		 * @param callback The callback function to remove.
-		 */
-		removeListener(eventName: string, callback: () => void): void {
-			if (isBrowser) target.removeEventListener(eventName, callback as EventListener);
-		}
-	};
-})();
+/**
+ * Dispatches an event with the given name.
+ * @param eventName The name of the event to dispatch.
+ */
+function dispatch(eventName: string): void {
+	if (isBrowser) target.dispatchEvent(new Event(eventName));
+}
 
-export default clientEventDispatcher;
+/**
+ * Listens for an event with the given name.
+ * @param eventName The name of the event to listen for.
+ * @param callback The callback function to invoke when the event occurs.
+ */
+function listen(eventName: string, callback: () => void): void {
+	if (isBrowser) target.addEventListener(eventName, callback as EventListener);
+}
+
+/**
+ * Removes a previously added event listener.
+ * @param eventName The name of the event.
+ * @param callback The callback function to remove.
+ */
+function removeListener(eventName: string, callback: () => void): void {
+	if (isBrowser) target.removeEventListener(eventName, callback as EventListener);
+}
+
+
+
+export default {
+	dispatch,
+	listen,
+	removeListener
+};
