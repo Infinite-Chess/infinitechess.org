6f689b01c81891246f6cb2444a325035af090467
Naviary2 <infinitechess.org@gmail.com>
2025-03-15 00:51:09 -0600

    Disabled checkmate for 8x8x8x8 Chess variant

--- src/client/scripts/esm/chess/logic/wincondition.js
+++ src/client/scripts/esm/chess/logic/wincondition.js
@@ -161,6 +161,7 @@ function wasLastMoveARoyalCapture(gamefile) {
  */
 function isCheckmateCompatibleWithGame(gamefile) {
 	if (gamefile.startSnapshot.pieceCount >= gamefileutility.pieceCountToDisableCheckmate) return false; // Too many pieces (checkmate algorithm takes too long)
+	if (gamefile.startSnapshot.slidingPossible.length > 16) return false; // If the game has more lines than this, then checkmate creates lag spikes.
 	if (gamefile.startSnapshot.playerCount > 2) return false; // 3+ Players allows for 1 player to open a discovered and a 2nd to capture a king. CHECKMATE NOT COMPATIBLE
 	if (moveutil.doesAnyPlayerGet2TurnsInARow(gamefile)) return false; // This also allows the capture of the king.
 	return true; // Checkmate compatible!
