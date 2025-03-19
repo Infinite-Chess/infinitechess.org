8e9a90cd5fd2be890a9fbdf86769e4e742426b62
Andreas Tsevas <andreas.tsevas@dlr.de>
2025-03-19 09:54:54 +0100

    Also set checkmates_beaten cookie on page refresh

--- src/server/api/PracticeProgress.ts
+++ src/server/api/PracticeProgress.ts
@@ -5,16 +5,76 @@
 
 import validcheckmates from "../../client/scripts/esm/chess/util/validcheckmates.js";
 // @ts-ignore
+import { ensureJSONString } from "../utility/JSONUtils.js";
+// @ts-ignore
 import { logEvents } from "../middleware/logEvents.js";
 // @ts-ignore
 import { getMemberDataByCriteria, updateMemberColumns } from '../database/memberManager.js';
 
 
 import type { CustomRequest } from "../../types.js";
-import type { Response } from "express";
+import type { Request, Response } from "express";
 
 
 // Functions -------------------------------------------------------------
+
+
+/**
+ * Middleware to set the preferences cookie for logged-in users based on their memberInfo cookie.
+ * Only sets the preferences cookie on HTML requests (requests without an origin header).
+ * 
+ * It is possible for the memberInfo cookie to be tampered with, but preferences can be public information anyway.
+ * We are reading the memberInfo cookie instead of verifying their session token
+ * because that could take a little bit longer as it requires a database look up.
+ * @param {Object} req - The Express request object.
+ * @param {Object} res - The Express response object.
+ * @param {Function} next - The Express next middleware function.
+ */
+function setPracticeProgressCookie(req: Request, res: Response, next: Function) {
+	if (!req.cookies) {
+		logEvents("req.cookies must be parsed before setting preferences cookie!", 'errLog.txt', { print: true });
+		return next();
+	}
+
+	// We don't have to worry about the request being for a resource because those have already been served.
+	// The only scenario this request could be for now is an HTML or fetch API request
+	// The 'is-fetch-request' header is a custom header we add on all fetch requests to let us know is is a fetch request.
+	if (req.headers['is-fetch-request'] === 'true' || !req.accepts('html')) return next(); // Not an HTML request (but a fetch), don't set the cookie
+
+	// We give everyone this cookie as soon as they login.
+	// Since it is modifiable by JavaScript it's possible for them to
+	// grab checkmates_beaten of other users this way, but there's no harm in that.
+	const memberInfoCookieStringified = req.cookies.memberInfo;
+	if (memberInfoCookieStringified === undefined) return next(); // No cookie is present, not logged in
+
+	let memberInfoCookie: {user_id: string, username: string}; // { user_id, username }
+	try {
+		memberInfoCookie = JSON.parse(memberInfoCookieStringified);
+	} catch (error) {
+		logEvents(`memberInfo cookie was not JSON parse-able when attempting to set checkmates_beaten cookie. Maybe it was tampered? The cookie: "${ensureJSONString(memberInfoCookieStringified)}" The error: ${(error as any).stack}`, 'errLog.txt', { print: true });
+		return next(); // Don't set the checkmates_beaten cookie, but allow their request to continue as normal
+	}
+
+	if (typeof memberInfoCookie !== "object") {
+		logEvents(`memberInfo cookie did not parse into an object when attempting to set checkmates_beaten cookie. Maybe it was tampered? The cookie: "${ensureJSONString(memberInfoCookieStringified)}"`, 'errLog.txt', { print: true });
+		return next(); // Don't set the checkmates_beaten cookie, but allow their request to continue as normal
+	}
+
+	const user_id = memberInfoCookie.user_id;
+	if (typeof user_id !== 'number') {
+		logEvents(`memberInfo cookie user_id property was not a number when attempting to set checkmates_beaten cookie. Maybe it was tampered? The cookie: "${ensureJSONString(memberInfoCookieStringified)}"`, 'errLog.txt', { print: true });
+		return next(); // Don't set the checkmates_beaten cookie, but allow their request to continue as normal
+	}
+
+	const checkmates_beaten = getCheckmatesBeaten(user_id); // Fetch their checkmates_beaten from the database
+	if (!checkmates_beaten) return next(); // No checkmates_beaten set for this user, or the user doesn't exist.
+
+	createPracticeProgressCookie(res, checkmates_beaten);
+	
+	// console.log(`Set checkmates_beaten cookie for member "${ensureJSONString(memberInfoCookie.username)}" for url: ` + req.url);
+
+	next();
+}
 
 
 /**
@@ -124,6 +184,7 @@ function postCheckmateBeaten(req: CustomRequest, res: Response): void {
 }
 
 export {
+	setPracticeProgressCookie,
 	createPracticeProgressCookie,
 	deletePracticeProgressCookie,
 	getCheckmatesBeaten,
