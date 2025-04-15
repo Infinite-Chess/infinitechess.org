286fb3401d9a6d0c165b3bbbe491b16a70224a64
Andreas Tsevas <tsevasa@gmail.com>
2025-04-16 00:34:02 +0200

    make server logging await

--- src/server/game/gamemanager/gamelogger.ts
+++ src/server/game/gamemanager/gamelogger.ts
@@ -61,10 +61,10 @@ async function logGame(game: Game) {
 	// TODO: Compute rating change in different file and update leaderboard
 
 	// 3. Enter the game into the player_games table
-	updatePlayerGamesTable(game, results.game_id); // TODO: Add support for rated games in here
+	await updatePlayerGamesTable(game, results.game_id); // TODO: Add support for rated games in here
 
-	// 4. Update the player stats table
-	updatePlayerStatsTable(game, results.game_id, victor);
+	// 4. Update the player_stats table
+	await updatePlayerStatsTable(game, results.game_id, victor);
 
 }
 
@@ -135,7 +135,7 @@ async function getICNOfGame(game: Game, metadata: MetaData): Promise<string | un
  * For each member, add an entry into player_games according to the results of this game.
  * If the game was ranked, also update the leaderboards table accordingly.
  */
-function updatePlayerGamesTable(game: Game, game_id: number) {
+async function updatePlayerGamesTable(game: Game, game_id: number) {
 	for (const playerStr in game.players) {
 		const player: Player = Number(playerStr) as Player;
 		const user_id = game.players[playerStr].identifier.user_id;
@@ -155,9 +155,9 @@ function updatePlayerGamesTable(game: Game, game_id: number) {
 		// Add game to player_games table in database
 		const results = addGameToPlayerGamesTable(options);
 		if (!results.success) {
-			logEvents('Failed to add game to player_games table after game. Check unloggedGames log.', 'errLog.txt', { print: true });
+			await logEvents('Failed to add game to player_games table after game. Check unloggedGames log.', 'errLog.txt', { print: true });
 			const errText = `Error when adding game to player_games when logging game: ${results.reason}  Member "${game.players[playerStr].identifier.member}", user_id "${user_id}". Their color: ${playerStr}. Game ID: ${game_id}. The game: ${gameutility.getSimplifiedGameString(game)}`;
-			logEvents(errText, 'unloggedGames.txt');
+			await logEvents(errText, 'unloggedGames.txt');
 			continue;
 		}
 	}
@@ -167,7 +167,7 @@ function updatePlayerGamesTable(game: Game, game_id: number) {
  * Update's each member's game stats in the player stats table according to the results of this game.
  * Such as game count, wins, losses, etc.
  */
-function updatePlayerStatsTable(game: Game, game_id: number, victor: Player | undefined) {
+async function updatePlayerStatsTable(game: Game, game_id: number, victor: Player | undefined) {
 
 	const playerMoveCounts: PlayerGroup<number> = getPlayerMoveCountsInGame(game);
 
@@ -207,9 +207,9 @@ function updatePlayerStatsTable(game: Game, game_id: number, victor: Player | un
 		const results = updatePlayerStatsColumns(user_id, player_stats);
 
 		if (!results.success) {
-			logEvents('Failed to increment player stats after game. Check unloggedGames log.', 'errLog.txt', { print: true });
+			await logEvents('Failed to increment player stats after game. Check unloggedGames log.', 'errLog.txt', { print: true });
 			const errText = `Error when UPDATING player stats when logging game: ${results.reason}  Member "${game.players[playerStr].identifier.member}", user_id "${user_id}". Their color: ${playerStr}. Game ID: ${game_id}. The game: ${gameutility.getSimplifiedGameString(game)}`;
-			logEvents(errText, 'unloggedGames.txt');
+			await logEvents(errText, 'unloggedGames.txt');
 			continue;
 		}
 	}
