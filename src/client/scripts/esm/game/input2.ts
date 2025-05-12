3ed24ccfb0b2474f3b57e763fce1c0c6536e6657
Naviary2 <infinitechess.org@gmail.com>
2025-05-11 19:49:05 -0600

    Stop debug log

--- src/client/scripts/esm/game/input2.ts
+++ src/client/scripts/esm/game/input2.ts
@@ -336,7 +336,7 @@ function CreateInputListener(element: HTMLElement | typeof document, { keyboard 
 			const MOVE_VPIXELS = e instanceof MouseEvent ? CLICK_THRESHOLDS.MOUSE.MOVE_VPIXELS : CLICK_THRESHOLDS.TOUCH.MOVE_VPIXELS; // CAN'T USE instanceof Touch because it's not defined in Safari!
 			if (distMoved < MOVE_VPIXELS && delta < MOVE_VPIXELS) { // Only register the double click drag if the mouse hasn't moved too far from its last click down.
 				targetButtonInfo.doubleClickDrag = true;
-				console.log("Mouse double click dragged: ", MouseNames[targetButton]);
+				// console.log("Mouse double click dragged: ", MouseNames[targetButton]);
 			}
 			// else console.log("Mouse double click MOVED TOO FAR: ", MouseNames[targetButton]);
 		} // ----------------
