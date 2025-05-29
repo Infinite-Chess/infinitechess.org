c26025c806cc79368ac255987bc3d577fea1e762
Naviary2 <infinitechess.org@gmail.com>
2025-05-28 22:46:16 -0600

    Indentation

--- src/server/game/gamemanager/ratingabuse.ts
+++ src/server/game/gamemanager/ratingabuse.ts
@@ -41,26 +41,26 @@ const GAME_INTERVAL_TO_MEASURE = 4;
  */
 function measurePlayerRatingAbuse(user_id: number, leaderboardId: number) {
 
-    /** 1. Early exit if it hasn't been {@link GAME_INTERVAL_TO_MEASURE} games since the last measure. */
+	/** 1. Early exit if it hasn't been {@link GAME_INTERVAL_TO_MEASURE} games since the last measure. */
 
 
-    /** 2. If they have net lost elo the past {@link GAME_INTERVAL_TO_MEASURE} games, no risk. */
+	/** 2. If they have net lost elo the past {@link GAME_INTERVAL_TO_MEASURE} games, no risk. */
 
-    const recentGames = getRecentNRatedGamesForUser(
-        user_id,
-        leaderboardId,
-        GAME_INTERVAL_TO_MEASURE,
+	const recentGames = getRecentNRatedGamesForUser(
+		user_id,
+		leaderboardId,
+		GAME_INTERVAL_TO_MEASURE,
         ['elo_change_from_game']
-    ) as { elo_change_from_game: number }[];
+	) as { elo_change_from_game: number }[];
     
-    const netRatingChange = recentGames.reduce(
-        (acc, g) => acc + g.elo_change_from_game,
-        0
-    );
+	const netRatingChange = recentGames.reduce(
+		(acc, g) => acc + g.elo_change_from_game,
+		0
+	);
 
-    if (netRatingChange <= 0) return; // They have lost elo. No cause for concern.
+	if (netRatingChange <= 0) return; // They have lost elo. No cause for concern.
 
-    // FINISH...
+	// FINISH...
 }
 
 
@@ -71,15 +71,21 @@ function measurePlayerRatingAbuse(user_id: number, leaderboardId: number) {
  * 
  * user_id PRIMARY KEY ON DELETE CASCADE
  * last_measure INTEGER NOT NULL -- Their game count we last measured them for rating abuse probability
- * ... some way to measure cheat report count?
- * 
- * OR, should all cheat reports just automatically notify me by email?
+ * ... some way to measure cheat report count? OR, should all cheat reports just automatically notify me by email?
  */
 
 /**
  * Add to table: player games
  * time_at_end NUMBER
+ * ISSUE: Previous logged games don't contain this information, maybe we can't have this cell.
  * 
  * Add to table: games
  * time_duration
+ * ISSUE: Previous logged games don't contain this information, maybe we can't have this cell.
  */
+
+
+export default {
+	measurePlayerRatingAbuse,
+};
+
