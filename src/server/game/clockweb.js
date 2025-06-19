10e3e9a7beb40fd7f4a77aa300b31499998d73da
Naviary2 <infinitechess.org@gmail.com>
2025-06-18 20:58:06 -0600

    Patched crash

--- src/server/game/clockweb.js
+++ src/server/game/clockweb.js
@@ -1,4 +1,7 @@
 
+// @ts-ignore
+import { DEV_BUILD } from "../config/config.js";
+
 /** These are the allowed time controls in production. */
 const validTimeControls = ['-','60+2','120+2','180+2','300+2','480+3','600+4','600+6','720+5','900+6','1200+8','1500+10','1800+15','2400+20']; 
 /** These are only allowed in development. */
