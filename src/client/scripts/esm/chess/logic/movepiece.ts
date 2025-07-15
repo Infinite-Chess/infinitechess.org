743771310f636871827b8ef4e20bb39ef88b8e62
Naviary Alt <tr.bomb20@gmail.com>
2025-07-15 17:19:47 -0600

    specialdetect => bigints

--- src/client/scripts/esm/chess/logic/movepiece.ts
+++ src/client/scripts/esm/chess/logic/movepiece.ts
@@ -69,7 +69,7 @@ type promotion = number;
 /** A special move tag for castling. */
 type castle = {
 	/** 1 => King castled right   2 => King castled left */
-	dir: 1 | -1,
+	dir: 1n | -1n,
 	/** The coordinate of the piece the king castled with, usually a rook. */
 	coord: Coords
 }
