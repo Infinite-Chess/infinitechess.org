7a182f096d54d6d7253e555369f90a66cce75d2d
Naviary2 <infinitechess.org@gmail.com>
2025-07-25 02:42:55 -0600

    lockFile property releases the locks even when encountering an error

--- src/server/utility/lockFile.ts
+++ src/server/utility/lockFile.ts
@@ -1,4 +1,6 @@
 
+// src/server/utility/lockFile.ts
+
 /**
  * This module extends the 'fs' module with methods
  * that lock a file while it is being read/written.
@@ -15,61 +17,36 @@ import fs from 'fs';
 // Locks the file while reading, then immediately unlocks and returns the data.
 // MUST BE CALLED WITH 'await' or this returns a promise!
 const readFile = async<D>(path: string, buffer: BufferEncoding = 'utf-8'): Promise<D> => {
-	let data: object;
+	let data: D | undefined;
 	await lockfile.lock(path)
 		.then((releaseFunc) => {
 			// Do something while the file is locked
 			try {
 				data = JSON.parse(fs.readFileSync(path, buffer));
-			} catch (e) { // Catching the error within JSON parsing like this allows us to unlock the file afterwards!
-				releaseFunc();
-				throw e;
+			} finally {
+				// Don't CATCH the error, but always release the lock, even if we encounter an error!
+				releaseFunc(); // Unlocks file
 			}
-			releaseFunc();
 		});
-	// @ts-ignore
-	return data;
+	return data!; // Guaranteed to be defined, since if it isn't, the function will have thrown anyway.
 };
 
 // Returns false when failed to lock/write file.
 // MUST BE CALLED WITH 'await' or this returns a promise!
-const writeFile = async(path: string, object: object): Promise<void> => {
+const writeFile = async(path: string, object: any): Promise<void> => {
 	await lockfile.lock(path)
 		.then((release) => {
 			// Do something while the file is locked
-			fs.writeFileSync(
-				path,
-				JSON.stringify(object, null, 1)
-				// , (err) => {
-				//     if (err) console.error(errorString, err);
-				//     else status = true;
-				// }
-			);
-			return release(); // Unlocks file
+			try {
+				fs.writeFileSync(path, JSON.stringify(object, null, 1));
+			} finally {
+				// Don't CATCH the error, but always release the lock, even if we encounter an error!
+				release(); // Unlocks file
+			}
 		});
-	return;
-};
-
-// Locks file before reading, editing, and saving.
-// MUST BE CALLED WITH 'await' or this returns a promise!
-// eslint-disable-next-line no-unused-vars
-const editFile = async<D>(path: string, callback: (data: D) => D, buffer: BufferEncoding = 'utf-8'): Promise<void> => {
-	await lockfile.lock(path)
-		.then((release) => {
-			// Do something while the file is locked
-			let data = JSON.parse(fs.readFileSync(path, buffer));
-			data = callback(data);
-			fs.writeFileSync(
-				path,
-				JSON.stringify(data, null, 1)
-			);
-			return release(); // Unlocks file
-		});
-	return;
 };
 
 export {
 	readFile,
-	writeFile,
-	editFile
+	writeFile
 };
