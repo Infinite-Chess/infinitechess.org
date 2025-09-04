7741208477ca0e90cdf683863a7ae982f2ca0ede
Naviary2 <infinitechess.org@gmail.com>
2025-09-03 20:35:24 -0600

    Gave Omega_Cubed a world border

--- src/client/scripts/esm/chess/variants/omega3generator.ts
+++ src/client/scripts/esm/chess/variants/omega3generator.ts
@@ -62,6 +62,8 @@ function genPositionOfOmegaCubed(): Map<CoordsKey, number> {
 	genBishopTunnel(startingPos, 15n, 6n, dist, dist);
 
 	surroundPositionInVoidBox(startingPos, { left: -500n, right: 500n, bottom: -500n, top: 500n });
+
+	// Bottom blip of pawns to prevent black rook from capturing them
 	startingPos.set(coordutil.getKeyFromCoords([499n,492n]), r.VOID + e.N);
 	startingPos.set(coordutil.getKeyFromCoords([7n,-500n]), r.PAWN + e.W);
 	startingPos.set(coordutil.getKeyFromCoords([8n,-500n]), r.PAWN + e.W);
@@ -69,14 +71,6 @@ function genPositionOfOmegaCubed(): Map<CoordsKey, number> {
 	startingPos.set(coordutil.getKeyFromCoords([10n,-500n]), r.PAWN + e.W);
 	startingPos.set(coordutil.getKeyFromCoords([11n,-500n]), r.PAWN + e.W);
 	startingPos.set(coordutil.getKeyFromCoords([12n,-500n]), r.PAWN + e.W);
-	startingPos.set(coordutil.getKeyFromCoords([6n,-501n]), r.VOID + e.N);
-	startingPos.set(coordutil.getKeyFromCoords([7n,-501n]), r.VOID + e.N);
-	startingPos.set(coordutil.getKeyFromCoords([8n,-501n]), r.VOID + e.N);
-	startingPos.set(coordutil.getKeyFromCoords([9n,-501n]), r.VOID + e.N);
-	startingPos.set(coordutil.getKeyFromCoords([10n,-501n]), r.VOID + e.N);
-	startingPos.set(coordutil.getKeyFromCoords([11n,-501n]), r.VOID + e.N);
-	startingPos.set(coordutil.getKeyFromCoords([12n,-501n]), r.VOID + e.N);
-	startingPos.set(coordutil.getKeyFromCoords([13n,-501n]), r.VOID + e.N);
 
 	// Bishop box that prevents black stalemate ideas
 	startingPos.set(coordutil.getKeyFromCoords([497n,-497n]), r.VOID + e.N);
