b056896855735e4ebc0421f4761cfd4a50d71d60
Naviary2 <infinitechess.org@gmail.com>
2025-09-10 16:49:19 -0600

    Fixed last move highlight not rendering

--- src/client/scripts/esm/game/rendering/highlights/highlights.ts
+++ src/client/scripts/esm/game/rendering/highlights/highlights.ts
@@ -19,6 +19,7 @@ import squarerendering from "./squarerendering.js";
 
 
 import type { Board } from "../../../chess/logic/gamefile.js";
+import type { Color } from "../../../util/math/math.js";
 
 
 /**
@@ -47,8 +48,8 @@ function highlightLastMove(boardsim: Board) {
 	const lastMove = moveutil.getCurrentMove(boardsim);
 	if (!lastMove) return; // Don't render if last move is undefined.
 
-	const color = preferences.getLastMoveHighlightColor();
-	const size = boardpos.getBoardScale();
+	const color: Color = preferences.getLastMoveHighlightColor();
+	const size: number = boardpos.getBoardScaleAsNumber();
 
 	squarerendering.genModel([lastMove.startCoords, lastMove.endCoords], color).render(undefined, undefined, { size });
 }
