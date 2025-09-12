f33a8daf77187f918a4d2d8f5a95b93fa40c4034
Naviary2 <infinitechess.org@gmail.com>
2025-09-12 01:36:54 -0600

    Playable region mask always renders, even when no world border

--- src/client/scripts/esm/game/rendering/border.ts
+++ src/client/scripts/esm/game/rendering/border.ts
@@ -12,7 +12,6 @@ import type { Board } from "../../chess/logic/gamefile.js";
 import meshes from "./meshes.js";
 import camera from "./camera.js";
 import primitives from "./primitives.js";
-import perspective from "./perspective.js";
 import bounds, { DoubleBoundingBox } from "../../util/math/bounds.js";
 import { createModel } from "./buffermodel.js";
 
@@ -24,18 +23,27 @@ import { createModel } from "./buffermodel.js";
  * playable area, just inside the world border.
  */
 function drawPlayableRegionMask(boardsim: Board): void {
-	const boundingBoxBD = meshes.expandTileBoundingBoxToEncompassWholeSquare(boardsim.playableRegion!);
-	const worldBox = meshes.applyWorldTransformationsToBoundingBox(boundingBoxBD);
 
 	const screenBox = camera.getRespectiveScreenBox();
-	// Cap the world box to the screen box.
-	// Fixes graphical glitches when the vertex data is beyond float32 range.
-	if (worldBox.left < screenBox.left) worldBox.left = screenBox.left;
-	if (worldBox.right > screenBox.right) worldBox.right = screenBox.right;
-	if (worldBox.bottom < screenBox.bottom) worldBox.bottom = screenBox.bottom;
-	if (worldBox.top > screenBox.top) worldBox.top = screenBox.top;
 
-	if (bounds.areBoxesDisjoint(worldBox, screenBox)) return; // No need to draw if playable area not on screen
+	let worldBox: DoubleBoundingBox;
+	if (boardsim.playableRegion) {
+
+		const boundingBoxBD = meshes.expandTileBoundingBoxToEncompassWholeSquare(boardsim.playableRegion);
+		worldBox = meshes.applyWorldTransformationsToBoundingBox(boundingBoxBD);
+
+		// Cap the world box to the screen box.
+		// Fixes graphical glitches when the vertex data is beyond float32 range.
+		if (worldBox.left < screenBox.left) worldBox.left = screenBox.left;
+		if (worldBox.right > screenBox.right) worldBox.right = screenBox.right;
+		if (worldBox.bottom < screenBox.bottom) worldBox.bottom = screenBox.bottom;
+		if (worldBox.top > screenBox.top) worldBox.top = screenBox.top;
+
+		if (bounds.areBoxesDisjoint(worldBox, screenBox)) return; // No need to draw if playable area not on screen
+	} else {
+		// No world border, just use the screen box
+		worldBox = screenBox;
+	}
 
 	const { left, right, bottom, top } = worldBox;
 	const vertexData = primitives.Quad_Color(left, bottom, right, top, [0,0,0,1]); // Color doesn't matter since it's a mask
