80f86f278e1eae24d45c0df511530aaf1a96ed4d
Naviary2 <infinitechess.org@gmail.com>
2025-09-16 23:40:17 -0600

    Added shaders.d.ts

---
+++ src/shaders.d.ts
@@ -0,0 +1,13 @@
+
+/*
+ * This tells TypeScript all .glsl imports are strings.
+ * 
+ * This can't be put inside globals.d.ts because TypeScript
+ * has a weird rule that global declarations must
+ * be in a separate file from module declarations.
+ */
+
+declare module '*.glsl' {
+	const content: string;
+	export default content;
+}
