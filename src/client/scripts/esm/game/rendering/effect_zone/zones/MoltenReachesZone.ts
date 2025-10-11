f528394b59ac519788bd520c8a48f37338001b75
Naviary2 <infinitechess.org@gmail.com>
2025-10-11 14:18:52 -0600

    Added blank color grade pass to Molten Reaches zone

--- src/client/scripts/esm/game/rendering/effect_zone/zones/MoltenReachesZone.ts
+++ src/client/scripts/esm/game/rendering/effect_zone/zones/MoltenReachesZone.ts
@@ -3,12 +3,15 @@ import { PostProcessPass } from "../../../../webgl/post_processing/PostProcessin
 import { ProgramManager } from "../../../../webgl/ProgramManager";
 import { Zone } from "../EffectZoneManager";
 import { HeatWavePass } from "../../../../webgl/post_processing/passes/HeatWavePass";
+import { ColorGradePass } from "../../../../webgl/post_processing/passes/ColorGradePass";
 
 
 export class MoltenReachesZone implements Zone {
 
 	/** The unique integer id this effect zone gets. */
 	readonly effectType: number = 4;
+
+	private colorGradePass: ColorGradePass;
 
 	/** Post processing effect creating heat waves. */
 	private heatWavePass: HeatWavePass | undefined = undefined;
@@ -20,6 +23,12 @@ export class MoltenReachesZone implements Zone {
 
 	constructor(programManager: ProgramManager, noise: Promise<WebGLTexture>) {
 		noise.then(texture => this.heatWavePass = new HeatWavePass(programManager, texture));
+
+		this.colorGradePass = new ColorGradePass(programManager);
+		// this.colorGradePass.saturation = 1.2;
+		// this.colorGradePass.gamma = 1.2;
+		// this.colorGradePass.tint = [1.0, 0.9, 0.9];
+		// this.colorGradePass.brightness = -0.1;
 	}
 
 
@@ -34,8 +43,9 @@ export class MoltenReachesZone implements Zone {
 	}
 
 	public getPasses(): PostProcessPass[] {
-		if (!this.heatWavePass) return [];
-		return [this.heatWavePass];
+		const activePasses: PostProcessPass[] = [this.colorGradePass];
+		if (this.heatWavePass) activePasses.push(this.heatWavePass);
+		return activePasses;
 	}
     
 	public fadeInAmbience(transitionDurationMillis: number): void {
