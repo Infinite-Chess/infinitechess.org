c9ea67f17d9ac5cf6f22c1457f023773b42fb124
Naviary Alt <tr.bomb20@gmail.com>
2025-11-19 03:24:46 +0000

    [Experimental] Decreased brightness of Echo Rift Zone

--- src/client/scripts/esm/game/rendering/effect_zone/zones/EchoRiftZone.ts
+++ src/client/scripts/esm/game/rendering/effect_zone/zones/EchoRiftZone.ts
@@ -33,7 +33,7 @@ export class EchoRiftZone implements Zone {
 
 	
 	/** The base brightness level around which the brightness will vary. */
-	private baseBrightness: number = -0.39;
+	private baseBrightness: number = -0.42;
 	/** How much the brightness will vary above and below the base brightness. */
 	private brightnessVariation: number = 0.07;
 
