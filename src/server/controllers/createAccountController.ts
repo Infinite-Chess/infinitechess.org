32de562c90017b1bbd5fa23283a6aa899b85431a
PilkeySEK <PilkeySEK@gmx.de>
2025-11-30 11:59:20 +0100

    remove commented out function

--- src/server/controllers/createAccountController.ts
+++ src/server/controllers/createAccountController.ts
@@ -252,14 +252,6 @@ async function doEmailValidation(string: string, req: Request, res: Response): P
 	return true;
 };
 
-/*
-function isValidEmail(string: string): boolean {
-	// Credit for the regex: https://stackoverflow.com/a/201378
-	// eslint-disable-next-line no-control-regex
-	const regex = /(?:[a-z0-9!#$%&'*+/=?^_`{|}~-]+(?:\.[a-z0-9!#$%&'*+/=?^_`{|}~-]+)*|"(?:[\x01-\x08\x0b\x0c\x0e-\x1f\x21\x23-\x5b\x5d-\x7f]|\\[\x01-\x09\x0b\x0c\x0e-\x7f])*")@(?:(?:[a-z0-9](?:[a-z0-9-]*[a-z0-9])?\.)+[a-z0-9](?:[a-z0-9-]*[a-z0-9])?|\[(?:(?:(2(5[0-5]|[0-4][0-9])|1[0-9][0-9]|[1-9]?[0-9]))\.){3}(?:(2(5[0-5]|[0-4][0-9])|1[0-9][0-9]|[1-9]?[0-9])|[a-z0-9-]*[a-z0-9]:(?:[\x01-\x08\x0b\x0c\x0e-\x1f\x21-\x5a\x53-\x7f]|\\[\x01-\x09\x0b\x0c\x0e-\x7f])+)\])/;
-	return regex.test(string);
-};
-*/
 /**
  * Checks an email address's MX records to see if it is valid
  */
