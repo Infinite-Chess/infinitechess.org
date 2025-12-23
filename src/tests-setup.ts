8bdd905a13eeec093663c77cb02d391d7afe4383
Naviary2 <infinitechess.org@gmail.com>
2025-12-22 22:33:53 -0700

    fix ts error

--- src/tests-setup.ts
+++ src/tests-setup.ts
@@ -2,5 +2,5 @@
 
 // Set up environment variables for testing.
 // Prevents `test` job failing due to missing secrets.
-process.env.ACCESS_TOKEN_SECRET = 'test_access_secret';
-process.env.REFRESH_TOKEN_SECRET = 'test_refresh_secret';
+process.env['ACCESS_TOKEN_SECRET'] = 'test_access_secret';
+process.env['REFRESH_TOKEN_SECRET'] = 'test_refresh_secret';
