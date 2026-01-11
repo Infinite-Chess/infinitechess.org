5db3ab7543bc7750d2f7fda4001285cb59883dca
Naviary2 <infinitechess.org@gmail.com>
2026-01-11 01:58:02 -0700

    cmt

--- src/tests-setup.ts
+++ src/tests-setup.ts
@@ -1,6 +1,6 @@
 // src/tests-setup.ts
 
 // Set up environment variables for testing.
-// Prevents `test` job failing due to missing secrets.
+// Prevents `test` workflow job failing due to missing secrets.
 process.env['ACCESS_TOKEN_SECRET'] = 'test_access_secret';
 process.env['REFRESH_TOKEN_SECRET'] = 'test_refresh_secret';
