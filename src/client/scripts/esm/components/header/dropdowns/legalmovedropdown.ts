9acc4754c88afaea32191292e20df868b1b931e3
Naviary2 <infinitechess.org@gmail.com>
2026-01-12 20:35:50 -0700

    Convert legalmovedropdown => ts

--- src/client/scripts/esm/components/header/dropdowns/legalmovedropdown.ts
+++ src/client/scripts/esm/components/header/dropdowns/legalmovedropdown.ts
@@ -6,14 +6,14 @@ import preferences from '../preferences.js';
 
 // Document Elements -------------------------------------------------------------------------
 
-const settingsDropdown = document.querySelector('.settings-dropdown');
+const settingsDropdown = document.querySelector('.settings-dropdown')!;
 
-const legalmoveDropdown = document.querySelector('.legalmove-dropdown');
+const legalmoveDropdown = document.querySelector('.legalmove-dropdown')!;
 // const dropdownItems = document.querySelectorAll(".legalmove-option");
-const legalmoveDropdownTitle = document.querySelector('.legalmove-dropdown .dropdown-title');
+const legalmoveDropdownTitle = document.querySelector('.legalmove-dropdown .dropdown-title')!;
 
-const squaresOption = document.querySelector('.legalmove-option.squares');
-const dotsOption = document.querySelector('.legalmove-option.dots');
+const squaresOption = document.querySelector('.legalmove-option.squares')!;
+const dotsOption = document.querySelector('.legalmove-option.dots')!;
 
 // Functions ---------------------------------------------------------------------------------
 
@@ -21,61 +21,61 @@ const dotsOption = document.querySelector('.legalmove-option.dots');
 	showCheckmarkOnSelectedOption();
 })();
 
-function showCheckmarkOnSelectedOption() {
+function showCheckmarkOnSelectedOption(): void {
 	const selectedLegalMovesOption = preferences.getLegalMovesShape(); // squares/dots
-	const targetCheckmark = document.querySelector(
+	const targetCheckmark = document.querySelector<HTMLElement>(
 		`.legalmove-option.${selectedLegalMovesOption} .checkmark`,
-	);
+	)!;
 	targetCheckmark.classList.remove('visibility-hidden');
 }
 
-function open() {
+function open(): void {
 	legalmoveDropdown.classList.remove('visibility-hidden'); // The stylesheet adds a short delay animation to when it becomes hidden
 	initListeners();
 	settingsDropdown.classList.add('transparent');
 }
-function close() {
+function close(): void {
 	legalmoveDropdown.classList.add('visibility-hidden'); // The stylesheet adds a short delay animation to when it becomes hidden
 	closeListeners();
 	settingsDropdown.classList.remove('transparent');
 }
 
-function initListeners() {
+function initListeners(): void {
 	legalmoveDropdownTitle.addEventListener('click', close);
 	squaresOption.addEventListener('click', toggleSquares);
 	dotsOption.addEventListener('click', toggleDots);
 }
-function closeListeners() {
+function closeListeners(): void {
 	legalmoveDropdownTitle.removeEventListener('click', close);
 	squaresOption.removeEventListener('click', toggleSquares);
 	dotsOption.removeEventListener('click', toggleDots);
 }
 
-function toggleSquares() {
+function toggleSquares(): void {
 	// console.log("Clicked squares");
 	preferences.setLegalMovesShape('squares');
 	hideAllCheckmarks();
-	const checkmark = document.querySelector('.legalmove-option.squares .checkmark');
+	const checkmark = document.querySelector<HTMLElement>('.legalmove-option.squares .checkmark')!;
 	checkmark.classList.remove('visibility-hidden');
 	dispatchLegalMoveChangeEvent();
 }
 
-function toggleDots() {
+function toggleDots(): void {
 	// console.log("Clicked dots");
 	preferences.setLegalMovesShape('dots');
 	hideAllCheckmarks();
-	const checkmark = document.querySelector('.legalmove-option.dots .checkmark');
+	const checkmark = document.querySelector<HTMLElement>('.legalmove-option.dots .checkmark')!;
 	checkmark.classList.remove('visibility-hidden');
 	dispatchLegalMoveChangeEvent();
 }
 
-function hideAllCheckmarks() {
+function hideAllCheckmarks(): void {
 	document.querySelectorAll('.legalmove-option .checkmark').forEach((checkmark) => {
 		checkmark.classList.add('visibility-hidden');
 	});
 }
 
-function dispatchLegalMoveChangeEvent() {
+function dispatchLegalMoveChangeEvent(): void {
 	// Dispatch a custom event for theme change so that any game code present can pick it up.
 	const themeChangeEvent = new CustomEvent('legalmove-shape-change');
 	document.dispatchEvent(themeChangeEvent);
