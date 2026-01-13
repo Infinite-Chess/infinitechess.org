24cbe0242f190609f9e2609c66bcedd90a89bb90
Naviary Alt <tr.bomb20@gmail.com>
2026-01-13 23:55:17 +0000

    Convert certOption -> ts

--- src/server/server.js
+++ src/server/server.js
@@ -13,10 +13,10 @@ import https from 'https';
 // Other imports
 import app from './app.js';
 import db from './database/database.js';
-import getCertOptions from './config/certOptions.js';
 import socketServer from './socket/socketServer.js';
 import { logAllGames } from './game/gamemanager/gamemanager.js';
 import { translateStaticTemplates } from './config/setupTranslations.js';
+import { getCertOptions } from './config/certOptions.js';
 
 translateStaticTemplates(); // Compiles static files. This part is NOT done for integration tests.
 
