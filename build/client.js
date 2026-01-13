56bafec1d369eb9818d291d70cd6a6808eb08d50
Naviary Alt <tr.bomb20@gmail.com>
2026-01-13 23:27:20 +0000

    Convert guide.js -> ts

--- build/client.js
+++ build/client.js
@@ -36,7 +36,7 @@ const ESMEntryPoints = [
 	'src/client/scripts/esm/views/news.ts',
 	'src/client/scripts/esm/views/createaccount.ts',
 	'src/client/scripts/esm/views/resetpassword.ts',
-	'src/client/scripts/esm/views/guide.js',
+	'src/client/scripts/esm/views/guide.ts',
 	'src/client/scripts/esm/views/admin.ts',
 	'src/client/scripts/esm/views/icnvalidator.ts',
 	'src/client/scripts/esm/game/chess/engines/engineCheckmatePractice.ts',
