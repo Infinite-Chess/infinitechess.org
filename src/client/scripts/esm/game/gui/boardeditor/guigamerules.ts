66d5470ec4e76ca7f119b5bf50836545d977d95b
Naviary Alt <tr.bomb20@gmail.com>
2026-01-21 01:17:35 +0000

    Disallow setting world border to values that would place pieces out of bounds

--- src/client/scripts/esm/game/gui/boardeditor/guigamerules.ts
+++ src/client/scripts/esm/game/gui/boardeditor/guigamerules.ts
@@ -15,6 +15,8 @@ import egamerules, { GameRulesGUIinfo } from '../../boardeditor/egamerules';
 import boardeditor from '../../boardeditor/boardeditor';
 import gameslot from '../../chess/gameslot';
 import guifloatingwindow from './guifloatingwindow';
+import boardutil from '../../../../../../shared/chess/util/boardutil';
+import bounds from '../../../../../../shared/util/math/bounds';
 
 // Elements ----------------------------------------------------------
 
@@ -257,6 +259,8 @@ function readGameRules(): void {
 		{ el: element_borderTop, val: element_borderTop.value },
 	];
 
+	const gamefile = gameslot.getGamefile()!;
+
 	const anyBorderSet = borderInputs.some((input) => input.val !== '');
 	if (!anyBorderSet) {
 		// All empty -> Valid (Undefined)
@@ -265,16 +269,16 @@ function readGameRules(): void {
 	} else {
 		// Must be valid integers or empty, and must be ascending
 		// Empty represents Infinity or -Infinity
-		const leftValid = !element_borderLeft.value || integerRegex.test(element_borderLeft.value);
-		const rightValid =
+		let leftValid = !element_borderLeft.value || integerRegex.test(element_borderLeft.value);
+		let rightValid =
 			!element_borderRight.value ||
 			(integerRegex.test(element_borderRight.value) &&
 				(!leftValid ||
 					!element_borderLeft.value ||
 					BigInt(element_borderRight.value) >= BigInt(element_borderLeft.value)));
-		const bottomValid =
+		let bottomValid =
 			!element_borderBottom.value || integerRegex.test(element_borderBottom.value);
-		const topValid =
+		let topValid =
 			!element_borderTop.value ||
 			(integerRegex.test(element_borderTop.value) &&
 				(!bottomValid ||
@@ -282,27 +286,45 @@ function readGameRules(): void {
 					BigInt(element_borderTop.value) >= BigInt(element_borderBottom.value)));
 
 		if (leftValid && rightValid && bottomValid && topValid) {
-			borderInputs.forEach((input) => input.el.classList.remove('invalid-input'));
+			// Initial values look valid
 			worldBorder = {
 				left: element_borderLeft.value ? BigInt(element_borderLeft.value) : null,
 				right: element_borderRight.value ? BigInt(element_borderRight.value) : null,
 				bottom: element_borderBottom.value ? BigInt(element_borderBottom.value) : null,
 				top: element_borderTop.value ? BigInt(element_borderTop.value) : null,
 			};
-		} else {
-			// Invalid: Either partial data or non-integer data or invalid ranges
-			// Mark invalid fields as invalid.
-			if (!leftValid) element_borderLeft.classList.add('invalid-input');
-			else element_borderLeft.classList.remove('invalid-input');
-			if (!rightValid) element_borderRight.classList.add('invalid-input');
-			else element_borderRight.classList.remove('invalid-input');
-			if (!bottomValid) element_borderBottom.classList.add('invalid-input');
-			else element_borderBottom.classList.remove('invalid-input');
-			if (!topValid) element_borderTop.classList.add('invalid-input');
-			else element_borderTop.classList.remove('invalid-input');
+			if (
+				worldBorder.left === null &&
+				worldBorder.right === null &&
+				worldBorder.bottom === null &&
+				worldBorder.top === null
+			)
+				worldBorder = undefined;
 
-			worldBorder = undefined;
+			// Further check if all pieces are within the border
+			if (worldBorder) {
+				const allCoords = boardutil.getCoordsOfAllPieces(gamefile.boardsim.pieces);
+				if (allCoords.some((coords) => !bounds.boxContainsSquare(worldBorder!, coords))) {
+					// One or more pieces are outside the border -> All invalid
+					leftValid = false;
+					rightValid = false;
+					bottomValid = false;
+					topValid = false;
+				}
+			}
 		}
+
+		// Mark invalid fields as invalid.
+		if (!leftValid) element_borderLeft.classList.add('invalid-input');
+		else element_borderLeft.classList.remove('invalid-input');
+		if (!rightValid) element_borderRight.classList.add('invalid-input');
+		else element_borderRight.classList.remove('invalid-input');
+		if (!bottomValid) element_borderBottom.classList.add('invalid-input');
+		else element_borderBottom.classList.remove('invalid-input');
+		if (!topValid) element_borderTop.classList.add('invalid-input');
+		else element_borderTop.classList.remove('invalid-input');
+
+		if (!leftValid || !rightValid || !bottomValid || !topValid) worldBorder = undefined;
 	}
 
 	const gameRules: GameRulesGUIinfo = {
@@ -327,7 +349,6 @@ function readGameRules(): void {
 		gameRules.worldBorder,
 	);
 
-	const gamefile = gameslot.getGamefile()!;
 	const mesh = gameslot.getMesh()!;
 	const edit: Edit = { changes: [], state: { local: [], global: [] } };
 
