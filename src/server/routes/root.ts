be80c69e17a3c366ea5bd6b57a712cc7db99a191
Naviary3 <tr.bomb20@gmail.com>
2026-02-02 03:45:10 -0700

    refactor: Convert root.js to root.ts

---
+++ src/server/routes/root.ts
@@ -0,0 +1,59 @@
+import express, { Request, Response } from 'express';
+import path from 'path';
+import { getLanguageToServe } from '../utility/translate.js';
+import { fileURLToPath } from 'node:url';
+
+const router = express.Router();
+const __dirname = path.dirname(fileURLToPath(import.meta.url));
+
+const htmlDirectory = path.join(__dirname, '../../../dist/client/views');
+
+/**
+ * Serves an HTML file based on the requested path and language.
+ * @param filePath - The relative file path to serve.
+ * @param localized - If the file is not localized to other languages.
+ * @returns Express middleware handler.
+ */
+const serveFile =
+	(filePath: string, localized: boolean = true) =>
+	(req: Request, res: Response) => {
+		const language: string = localized ? getLanguageToServe(req) : '';
+		const file: string = path.join(htmlDirectory, language, filePath);
+		/**
+		 * sendFile() will AUTOMATICALLY check if the file's Last-Modified
+		 * value is after the request's 'If-Modified-Since' header...
+		 *
+		 * If so, it will send 200 OK with the updated file content!
+		 *
+		 * Otherwise, it sends 304 Not Modified, signaling the client
+		 * to use their cached version for another duration of the
+		 * max-age property of the Cache-Control header we send!
+		 */
+		res.sendFile(file);
+	};
+
+// Regular pages
+router.get('^/$|/index(.html)?', serveFile('index.html'));
+router.get('/credits(.html)?', serveFile('credits.html'));
+router.get('/play(.html)?', serveFile('play.html'));
+router.get('/guide(.html)?', serveFile('guide.html'));
+router.get('/news(.html)?', serveFile('news.html'));
+router.get('/leaderboard(.html)?', serveFile('leaderboard.html'));
+router.get('/login(.html)?', serveFile('login.html'));
+router.get('/createaccount(.html)?', serveFile('createaccount.html'));
+
+router.get('/reset-password/:token', serveFile('resetpassword.html'));
+
+router.get('/termsofservice(.html)?', serveFile('termsofservice.html'));
+router.get('/member(.html)?/:member', serveFile('member.html'));
+router.get('/admin(.html)?', serveFile('admin.html'));
+router.get('/icnvalidator(.html)?', serveFile('icnvalidator.html', false));
+
+// Error pages
+router.get('/400(.html)?', serveFile('errors/400.html', true));
+router.get('/401(.html)?', serveFile('errors/401.html', true));
+router.get('/404(.html)?', serveFile('errors/404.html', true));
+router.get('/409(.html)?', serveFile('errors/409.html', true));
+router.get('/500(.html)?', serveFile('errors/500.html', true));
+
+export { router };
