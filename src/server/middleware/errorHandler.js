35e13dfa5da27df4d364d8638909c7163d6970ab
Naviary2 <infinitechess.org@gmail.com>
2026-02-01 18:06:06 -0700

    Added last line error catching in middleware error handler

--- src/server/middleware/errorHandler.js
+++ src/server/middleware/errorHandler.js
@@ -1,13 +1,20 @@
 import { getTranslationForReq } from '../utility/translate.js';
 import { logEventsAndPrint } from './logEvents.js';
 
-function errorHandler(_err, _req, _res, _next) {
-	const errMessage = `${_err.stack}`;
-	logEventsAndPrint(errMessage, 'errLog.txt');
+function errorHandler(err, req, res, _next) {
+	try {
+		const errMessage = `${err.stack}`;
+		logEventsAndPrint(errMessage, 'errLog.txt');
 
-	// This sends back to the browser the error, instead of the ENTIRE stack which is PRIVATE.
-	const messageForClient = getTranslationForReq('server.javascript.ws-server_error', _req);
-	_res.status(500).send(messageForClient); // 500: Server error
+		// This sends back to the browser the error, instead of the ENTIRE stack which is PRIVATE.
+		const messageForClient = getTranslationForReq('server.javascript.ws-server_error', req);
+		res.status(500).send(messageForClient); // 500: Server error
+	} catch (error) {
+		// Last line of defense if an error occurs in the middleware error catcher
+		const errMessage = error instanceof Error ? error.stack : String(error);
+		console.error('Critical error in errorHandler middleware:', errMessage);
+		res.status(500).send('Critical server error.');
+	}
 }
 
 export default errorHandler;
