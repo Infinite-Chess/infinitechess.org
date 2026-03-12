dade7435815e04aa9000258435442f731330616d
Naviary2 <infinitechess.org@gmail.com>
2026-03-12 00:58:58 -0600

    Oops this is meant to stay at 15m

--- src/shared/util/tokenConfig.ts
+++ src/shared/util/tokenConfig.ts
@@ -6,8 +6,8 @@
  */
 
 /** The expiration duration of access tokens, in milliseconds. */
-// const ACCESS_TOKEN_EXPIRY_MILLIS: number = 1000 * 60 * 15; // 15 minutes
-const ACCESS_TOKEN_EXPIRY_MILLIS: number = 1000 * 20; // 20 seconds, for testing purposes.
+const ACCESS_TOKEN_EXPIRY_MILLIS: number = 1000 * 60 * 15; // 15 minutes
+// const ACCESS_TOKEN_EXPIRY_MILLIS: number = 1000 * 20; // 20 seconds, for testing purposes.
 
 export default {
 	ACCESS_TOKEN_EXPIRY_MILLIS,
