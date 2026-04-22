4c817a4d2e94b4a0291445c286d640b389763fad
Naviary2 <infinitechess.org@gmail.com>
2026-04-22 14:32:50 -0600

    Revert "experiment: check if the file path is doubly-added" (no issues)
    This reverts commit 43a95b84cb38b007c086039e08a99e269fde2865.

--- src/client/scripts/esm/util/tooltips.ts
+++ src/client/scripts/esm/util/tooltips.ts
@@ -338,7 +338,7 @@ function resetSuppressTimer(target: HTMLElement, state: TooltipState, direction:
 	state.suppressTimer = window.setTimeout(() => {
 		state.suppressed = false;
 		if (state.isHovering && !state.isHolding) tryShow(target, state, direction);
-	}, SUPPRESS_COOLDOWN_MILLIS * 1.0);
+	}, SUPPRESS_COOLDOWN_MILLIS);
 }
 
 // Delegated event listeners ------------------------------------------------------------
