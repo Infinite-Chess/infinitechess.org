afbe4f9d8e1f911b43f28430d7b044636c32b60b
Naviary2 <infinitechess.org@gmail.com>
2026-05-13 00:56:22 -0600

    Add more example seeks

--- src/client/scripts/esm/views/index/index.ts
+++ src/client/scripts/esm/views/index/index.ts
@@ -10,6 +10,7 @@ import './gameSetupModal.js';
 // DELETE ONCE CONNECTED TO BACKEND AND RENDERING REAL SEEKS.
 
 const EXAMPLE_SEEKS: LobbySeek[] = [
+	// Standard example
 	{
 		id: 'seek1',
 		tag: 'tag1',
@@ -19,17 +20,60 @@ const EXAMPLE_SEEKS: LobbySeek[] = [
 			rating: { value: 1758, confident: false },
 		},
 		color: null,
-		variant: { group: 'standard', name: 'Classical' },
-		time: '300+5',
+		variant: { group: 'standard', code: 'Classical' },
+		time: '1500+5',
 		mode: 'rated',
 	},
+	// Horde example
 	{
 		id: 'seek2',
 		tag: 'tag2',
 		player: { type: 'guest', username: '(Guest)' },
 		color: p.WHITE,
-		variant: { group: 'horde', name: 'Pawn_Horde' },
+		variant: { group: 'horde', code: 'Pawn_Horde' },
 		time: '-',
+		mode: 'casual',
+	},
+	// 4D example
+	{
+		id: 'seek3',
+		tag: 'tag3',
+		player: {
+			type: 'player',
+			username: '4DEnthusiast',
+			rating: { value: 1900, confident: true },
+		},
+		color: p.BLACK,
+		variant: { group: '4D', code: '5D_Chess' },
+		time: '900+10',
+		mode: 'rated',
+	},
+	// Showcase example
+	{
+		id: 'seek4',
+		tag: 'tag4',
+		player: {
+			type: 'player',
+			username: 'ChessMaster3000',
+			rating: { value: 2200, confident: true },
+		},
+		color: null,
+		variant: { group: 'showcase', code: 'Omega_Squared' },
+		time: '180+1',
+		mode: 'rated',
+	},
+	// Custom example
+	{
+		id: 'seek5',
+		tag: 'tag5',
+		player: {
+			type: 'player',
+			username: 'CustomVariantFan',
+			rating: { value: 1500, confident: false },
+		},
+		color: null,
+		variant: { group: 'custom', name: 'Custom Variant' },
+		time: '60+1',
 		mode: 'casual',
 	},
 ];
