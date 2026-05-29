223332b674b6317ed6ebe7cd249d30944e7af2cd
Naviary <163621561+Naviary2@users.noreply.github.com>
2026-05-29 00:15:25 +0000

    Convert lobby subs to set

--- src/server/game/invitesmanager/invitessubscribers.ts
+++ src/server/game/invitesmanager/invitessubscribers.ts
@@ -13,18 +13,15 @@ import type { CustomWebSocket } from '../../socket/socketUtility.js';
 import { memberInfoEq } from './inviteutility.js';
 import { sendSocketMessage } from '../../socket/sendSocketMessage.js';
 
-/**
- * List of clients currently subscribed to invites list events, with their
- * socket id for the keys, and their socket for the value.
- */
-const subscribedClients: Map<string, CustomWebSocket> = new Map();
+/** Set of clients currently subscribed to invites list events. */
+const subscribedClients: Set<CustomWebSocket> = new Set();
 
 const printSubscriberCount = false;
 
 /**
  * Returns an iterator over all sockets currently subscribed to the invites list.
  */
-function getInviteSubscribers(): MapIterator<CustomWebSocket> {
+function getInviteSubscribers(): SetIterator<CustomWebSocket> {
 	return subscribedClients.values();
 }
 
@@ -34,7 +31,7 @@ function getInviteSubscribers(): MapIterator<CustomWebSocket> {
  * @param message - The message contents
  */
 function broadcastToAllInviteSubs(action: string, message: any): void {
-	for (const ws of subscribedClients.values()) {
+	for (const ws of subscribedClients) {
 		sendSocketMessage(ws, 'lobby', action, message); // In order: socket, sub, action, value
 	}
 }
@@ -43,11 +40,10 @@ function broadcastToAllInviteSubs(action: string, message: any): void {
  * Adds a new socket to the invite subscriber list.
  */
 function addSocketToInvitesSubs(ws: CustomWebSocket): void {
-	const socketID = ws.metadata.id;
-	if (subscribedClients.has(socketID))
+	if (subscribedClients.has(ws))
 		return console.error('Cannot sub socket to invites list because they already are!');
 
-	subscribedClients.set(socketID, ws);
+	subscribedClients.add(ws);
 	ws.metadata.subscriptions.lobby = true;
 
 	if (printSubscriberCount) console.log(`Invites subscriber count: ${subscribedClients.size}`);
@@ -61,10 +57,9 @@ function removeSocketFromInvitesSubs(ws: CustomWebSocket): void {
 	if (!ws)
 		return console.error("Can't remove socket from invites subs list because it's undefined!");
 
-	const socketID = ws.metadata.id;
-	if (!subscribedClients.has(socketID)) return; // Cannot unsub socket from invites list because they aren't subbed.
+	if (!subscribedClients.has(ws)) return; // Cannot unsub socket from invites list because they aren't subbed.
 
-	subscribedClients.delete(socketID);
+	subscribedClients.delete(ws);
 	delete ws.metadata.subscriptions.lobby;
 
 	if (printSubscriberCount) console.log(`Invites subscriber count: ${subscribedClients.size}`);
@@ -80,7 +75,7 @@ function getSubscriberCount(): number {
  * @returns true if the member or browser ID has at least one active connection, false otherwise.
  */
 function doesUserHaveActiveConnection(info: AuthMemberInfo): boolean {
-	for (const ws of subscribedClients.values()) {
+	for (const ws of subscribedClients) {
 		if (memberInfoEq(ws.metadata.memberInfo, info)) return true;
 	}
 	return false;
