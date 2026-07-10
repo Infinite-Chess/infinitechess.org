5ee80f795756bc4c527db73303d63a3f4930ae6c
Naviary2 <infinitechess.org@gmail.com>
2026-07-10 00:22:03 -0600

    Don't update .tempo of player bars on analysis page

--- src/client/scripts/esm/game/gui/guiclock.ts
+++ src/client/scripts/esm/game/gui/guiclock.ts
@@ -97,7 +97,7 @@ function set(basegame: GameFile): void {
 	const topPlayer = bottomPlayer === p.WHITE ? p.BLACK : p.WHITE;
 	element_timers = { [bottomPlayer]: bars.bottom, [topPlayer]: bars.top };
 
-	updateTempo(basegame); // Highlight whoever's turn it is, even in untimed games.
+	if (gamesession.getGameType() !== 'analysis') updateTempo(basegame); // Highlight whoever's turn it is, even in untimed games.
 	if (basegame.untimed) return;
 	updateTextContent(basegame.clocks);
 }
@@ -136,7 +136,6 @@ function showViewedMoveClockStamps(basegame: GameFile): void {
 	}
 
 	updateTextContent({ startTime: basegame.clocks.startTime, currentTime });
-	updateTempo(basegame);
 }
 
 /** Highlights the bar of the player whose turn it is via `.tempo`. */
