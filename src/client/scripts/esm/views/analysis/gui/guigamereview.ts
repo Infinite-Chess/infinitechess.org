a318a4cbdb18a7eb0f39caf7407c8e5dabd237f7
Naviary2 <infinitechess.org@gmail.com>
2026-07-21 01:53:11 -0600

    Success: eslint warns fail the job. Fix the lint warn now.

--- src/client/scripts/esm/views/analysis/gui/guigamereview.ts
+++ src/client/scripts/esm/views/analysis/gui/guigamereview.ts
@@ -29,7 +29,6 @@ import gameslot from '../../../game/chess/gameslot.js';
 import gamereview from '../gamereview.js';
 import guimovetree from './guimovetree.js';
 import { GameBus } from '../../../game/GameBus.js';
-import gamesession from '../../../game/chess/gamesession.js';
 
 // Elements ---------------------------------------------------------------------------
 
